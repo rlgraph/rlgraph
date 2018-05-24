703933206f31c5f448ddc1467421f1939c7a6bcd
Sven <svenmika1977@gmail.com>
2018-05-24 22:15:34 +0200

    - Action Head (still in scribble state). - Stubs for Noise-Components.

--- yarl/components/action_heads/action_head.py
+++ yarl/components/action_heads/action_head.py
@@ -17,35 +17,87 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from yarl.components import Component
+import tensorflow as tf
+
+from yarl.components import Component, EXPOSE_INS
 
 
-class ActionHead(Component):
-    def __init__(self, action_space,
-                 epsilon_component=None, epsilon_strategy="greedy", noise_component=None,
-                 scope="action-head", **kwargs):
-        super(ActionHead, self).__init__(scope=scope, **kwargs)
-
-        self.define_inputs("nn_output", "time_step")
-        self.define_outputs("action")
-        self.add_computation(["nn_output", "time_step"], "action", self._computation_act)
-
-    def _computation_act(self, nn_output, time_step):
+class ActionHeadComponent(Component):
+    def __init__(self, add_softmax=False, picking_strategy="max-likelihood", epsilon_component=None,
+                 noise_spec=None, scope="action-head", **kwargs):
         """
         Args:
-            nn_output (DataOp): The output from the neural network (policy, Q-net, etc..).
+            #action_space (Space): The action Space. Outputs of the "action" Socket must be members of this Space.
+            add_softmax (bool): Whether to softmax the NN outputs (before further processing them).
+            picking_strategy (str): One of: "max-likelihood", "random", "sample".
+            epsilon_component (Component): The sub-Component for epsilon-based exploration (e.g. a DecayComponent).
+            #noise_spec (dict): The specification dict for a noise generator that adds noise to the NN's output.
+        """
+        super(ActionHeadComponent, self).__init__(scope=scope, **kwargs)
+
+        self.add_softmax = add_softmax
+
+        #self.action_space = action_space
+        self.picking_strategy = picking_strategy
+
+        self.epsilon_component = epsilon_component
+        #self.noise_spec = noise_spec
+
+        # The Bernoulli Distribution to check, whether to do (epsilon) exploration or not.
+        #self.epsilon_distribution =
+        # The Distribution to sample (or pick) actions from.
+        self.action_distribution = None
+
+        self.define_inputs("time_step", "nn_output")
+        self.define_outputs("action")
+
+        # Add epsilon Component and connect accordingly.
+        if self.epsilon_component is not None:
+            self.add_component(self.epsilon_component, expose=EXPOSE_INS)
+            self.connect("time_step", self.epsilon_component.get_input("time_step"))
+            self.connect(self.epsilon_component.get_input("value"), "epsilon")
+            self.add_computation(["time_step", "nn_output", self.epsilon_component.get_output("epsilon")],
+                                 "action", self._computation_act)
+        # Else, set epsilon to 0.0 (no epsilon-based exploration).
+        else:
+            self.add_computation(["time_step", "nn_output", 0.0], "action", self._computation_act)
+
+    def create_variables(self, input_spaces):
+        pass
+        #nn_action_space = input_spaces["nn_output"]
+        #if self.epsilon_component is not None:
+
+
+        # Now that we know the NN's output shape, we can generate our distributions.
+        #if self.epsilon_component is not None:
+            #self.epsilon_distribution
+
+    def _computation_act(self, time_step, nn_output, epsilon):
+        """
+        Args:
             time_step (DataOp): The time-step information needed to figure out the amount and nature of exploration.
+            nn_output (DataOp): The output from the neural network (policy, Q-net, etc..).
 
         Returns:
             DataOp: The DataOp representing the action. This will match the shape of self.action_space.
         """
-        raise NotImplementedError
+        # Pass NN-output through softmax first?
+        if self.add_softmax:
+            nn_output = tf.nn.softmax(nn_output)  # axis -1 ok?
+
+        # Sample our epsilon-defined Bernoulli distribution.
+        do_explore = False
+        if self.epsilon_component is not None:
+            pass
+            #do_explore =
+
+
 
 """        
-        ActionHead
+        ActionHeadComponent
     __init__(action-space, epsilon-comp=epsilon-component if None, no epsilon;
         alternatively: noise-component to be added to continuous action output?
-        action-pick=[greedy|sample|random])
+        epsilon-strategy=[greedy|sample|random])
     in: network-output (q-values or probs), time-step
     out: "act"
     _computation_act: use sub-exploration-component with connected  time-step to calculate epsilon:
