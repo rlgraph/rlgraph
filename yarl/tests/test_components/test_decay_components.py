5590b73f540e92cc98786415d4982b4852c68c40
Sven <svenmika1977@gmail.com>
2018-06-03 13:26:39 +0200

    - Fix exponential decay test case.

--- yarl/tests/test_components/test_decay_components.py
+++ yarl/tests/test_components/test_decay_components.py
@@ -35,7 +35,6 @@ class TestDecayComponents(unittest.TestCase):
     time_step_space = IntBox(1000, add_batch_rank=False)
 
     def test_linear_decay(self):
-        # The Component to test.
         decay_component = LinearDecay(from_=1.0, to_=0.0, start_timestep=100, num_timesteps=100)
         test = ComponentTest(component=decay_component, input_spaces=dict(time_step=self.time_step_space))
 
@@ -47,14 +46,13 @@ class TestDecayComponents(unittest.TestCase):
             test.test(out_socket_name="value", inputs=i, expected_outputs=e)
 
     def test_exponential_decay(self):
-        # The Component to test.
-        decay_component = ExponentialDecay(from_=1.0, to_=0.0, start_timestep=100, num_timesteps=100)
+        decay_component = ExponentialDecay(from_=1.0, to_=0.0, start_timestep=0, num_timesteps=100, half_life=50)
         test = ComponentTest(component=decay_component, input_spaces=dict(time_step=self.time_step_space))
 
         # Values to pass as single items.
-        input_ = np.array([0, 1, 2, 25, 50, 100, 110, 112, 120, 130, 150, 180, 190, 195, 200, 201, 210, 250, 1000])
-        expected = np.array([1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.9, 0.88, 0.8, 0.7, 0.5, 0.2, 0.1, 0.05, 0.0, 0.0, 0.0,
-                             0.0, 0.0])
+        input_ = np.array([0, 1, 2, 25, 50, 75, 80, 90, 99, 100])
+        expected = np.array([1.0, 0.9862327, 0.97265494, 0.70710677, 0.5,
+                             0.35355338, 0.329877, 0.2871746, 0.25348988, 0.0])
         for i, e in zip(input_, expected):
             test.test(out_socket_name="value", inputs=i, expected_outputs=e)
 
