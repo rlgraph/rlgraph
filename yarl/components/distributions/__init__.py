502dd71e8320eac17e321eb96d36f8751174ed02
Michael Schaarschmidt <mi.schaarschmidt@gmail.com>
2018-06-05 20:40:50 +0100

    Fix distribution imports.

--- yarl/components/distributions/__init__.py
+++ yarl/components/distributions/__init__.py
@@ -17,8 +17,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from .distributions import Distribution, Bernoulli, Categorical, Normal, Beta
-from .nn_output_cleanup import NNOutputCleanup
+from yarl.components.distributions import Distribution, Bernoulli, Categorical, Normal, Beta
+from yarl.components.distributions.nn_output_cleanup import NNOutputCleanup
 
 Distribution.__lookup_classes__ = dict(
     bernoulli=Bernoulli,
