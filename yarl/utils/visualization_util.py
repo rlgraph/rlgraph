fda66764b1da2f90140c46b94f4da07008fa2f32
Kai Fricke <k@ifricke.com>
2018-06-07 19:11:01 +0200

    visualization: added styling for spaces

--- yarl/utils/visualization_util.py
+++ yarl/utils/visualization_util.py
@@ -41,8 +41,10 @@ def get_graph_markup(component, level=0):
         markup = " " * 4 * level + "subgraph {}\n".format(component.name)
     elif level == 0:
         markup = "graph TD\n"
-        markup += "classDef input_socket fill:#9ff,stroke:#333,stroke-width:4px;\n"
-        markup += "classDef output_socket fill:#f9f,stroke:#333,stroke-width:4px;\n\n"
+        markup += "classDef input_socket fill:#9ff,stroke:#333,stroke-width:2px;\n"
+        markup += "classDef output_socket fill:#f9f,stroke:#333,stroke-width:2px;\n"
+        markup += "classDef space fill:#999,stroke:#333,stroke-width:2px;\n"
+        markup += "\n"
     else:
         raise YARLError("Invalid component indentation level {}".format(level))
 
@@ -69,6 +71,7 @@ def get_graph_markup(component, level=0):
     # Collect connections by looping through all incoming connections.
     # All outgoing connections should be incoming connections of another socket, so we don't need to loop through them.
     connections = list()
+    markup_spaces = list()
     for socket in all_sockets:
         for incoming_connection in socket.incoming_connections:
             if isinstance(incoming_connection, Socket):
@@ -77,8 +80,10 @@ def get_graph_markup(component, level=0):
                     "socket_{}".format(hash(socket))
                 ))
             elif isinstance(incoming_connection, Space):
+                # Add spaces to markup (we only know about them because of their connections).
                 markup += " " * 4 * (level + 1) + "space_{hash}(\"{name}\")\n".format(hash=hash(incoming_connection),
                                                                                       name=str(incoming_connection))
+                markup_spaces.append("space_{hash}".format(hash=hash(incoming_connection)))
                 connections.append((
                     "space_{}".format(hash(incoming_connection)),
                     "socket_{}".format(hash(socket))
@@ -91,6 +96,10 @@ def get_graph_markup(component, level=0):
     # Add style class `output_socket` to the output sockets
     if markup_output_sockets:
         markup += " " * 4 * (level + 1) + "class {} output_socket;\n".format(','.join(markup_output_sockets))
+
+    # Add style class `space` to the spaces
+    if markup_spaces:
+        markup += " " * 4 * (level + 1) + "class {} space;\n".format(','.join(markup_spaces))
 
     markup += "\n"
 
