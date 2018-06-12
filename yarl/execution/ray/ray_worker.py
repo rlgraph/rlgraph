368f370c3afada352bdece0dc447f55da6e611ca
Michael Schaarschmidt <mi.schaarschmidt@gmail.com>
2018-06-12 19:06:37 +0100

    Ray worker sampling basics.

--- yarl/execution/ray/ray_worker.py
+++ yarl/execution/ray/ray_worker.py
@@ -17,25 +17,122 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+from six.moves import xrange
+import time
 import ray
 
-from yarl.execution import SingleThreadedWorker
+from yarl import distributed_backend
+from yarl.execution import Worker
 
 
 @ray.remote
-class RayWorker(SingleThreadedWorker):
+class RayWorker(Worker):
     """
     Ray wrapper for single threaded worker, provides further api methods to interact
     with the agent used in the worker.
-
     """
     def __init__(self, **kwargs):
+        # Should be set
+        assert distributed_backend == "ray"
         super(RayWorker, self).__init__(**kwargs)
 
     # Remote functions to interact with this workers agent.
     def call_agent_op(self, op, inputs=None):
         self.agent.call_graph_op(op, inputs)
-    #
+
+    def execute_and_get_timesteps(
+        self,
+        num_timesteps,
+        max_timesteps_per_episode=0,
+        deterministic=False,
+        break_on_terminal=False
+    ):
+        """
+        Collects and returns timestep experience.
+
+        Args:
+            break_on_terminal (Optional[bool]): If true, breaks when a terminal is encountered. If false,
+                executes exactly 'num_timesteps' steps.
+        """
+        start = time.monotonic()
+        timesteps_executed = 0
+        episodes_executed = 0
+        env_frames = 0
+        states = []
+        actions = []
+        rewards = []
+        terminals = []
+        episode_rewards = []
+        state, _, _, _ = self.environment.reset()
+
+        while timesteps_executed < num_timesteps:
+            # The reward accumulated over one episode.
+            episode_reward = 0
+
+            # Whether the episode has terminated.
+            terminal = False
+            while True:
+                action = self.agent.get_action(states=state, deterministic=deterministic)
+
+                states.append(state)
+                actions.append(action)
+
+                # Accumulate the reward over n env-steps (equals one action pick). n=self.repeat_actions
+                reward = 0
+                for _ in xrange(self.repeat_actions):
+                    state, step_reward, terminal, info = self.environment.step(actions=action)
+                    env_frames += 1
+                    reward += step_reward
+
+                rewards.append(reward)
+                terminals.append(terminal)
+
+                if terminal:
+                    # Just return all samples collected so far.
+                    if break_on_terminal:
+                        total_time = (time.monotonic() - start) or 1e-10
+                        return dict(
+                            runtime=total_time,
+                            # Agent act/observe throughput.
+                            timesteps_executed=timesteps_executed,
+                            ops_per_second=(timesteps_executed / total_time),
+                            # Env frames including action repeats.
+                            env_frames=env_frames,
+                            env_frames_per_second=(env_frames / total_time),
+                            episodes_executed=1,
+                            episodes_per_minute=(1 / (total_time / 60)),
+                            episode_rewards=episode_reward,
+                            states=states,
+                            actions=actions,
+                            rewards=rewards,
+                            terminals=terminals
+                        )
+                    else:
+                        break
+
+            episodes_executed += 1
+            episode_rewards.append(episode_reward)
+
+        # Otherwise return when all time steps done
+        total_time = (time.monotonic() - start) or 1e-10
+        return dict(
+            runtime=total_time,
+            # Agent act/observe throughput.
+            timesteps_executed=timesteps_executed,
+            ops_per_second=(timesteps_executed / total_time),
+            # Env frames including action repeats.
+            env_frames=env_frames,
+            env_frames_per_second=(env_frames / total_time),
+            episodes_executed=episodes_executed,
+            episodes_per_minute=(1 / (total_time / 60)),
+            episode_rewards=episode_rewards,
+            states=states,
+            actions=actions,
+            rewards=rewards,
+            terminals=terminals
+        )
+
+    # TODO decide later if using separate methods here for apex
     # def set_weights(self, weights):
     #     self.agent.call_graph_op("set_weights", weights)
     #
