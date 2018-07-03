ea13ac9b71ff168b7813b76d9089eab785750fb8
Michael Schaarschmidt <mi.schaarschmidt@gmail.com>
2018-07-03 16:14:34 +0100

    update api, comment out and add todos on core tests.

--- yarl/tests/test_core/test_graph_fns.py
+++ yarl/tests/test_core/test_graph_fns.py
@@ -24,7 +24,7 @@ import unittest
 from yarl.tests import ComponentTest
 from yarl.utils import root_logger
 import yarl.spaces as spaces
-from yarl.tests.dummy_components import FlattenSplitDummy, NoFlattenNoSplitDummy, OnlyFlattenDummy
+from yarl.tests.dummy_components import *
 
 
 class TestGraphFns(unittest.TestCase):
@@ -34,120 +34,122 @@ class TestGraphFns(unittest.TestCase):
     """
     root_logger.setLevel(level=logging.INFO)
 
-    def test_2_containers_flattening_splitting(self):
-        """
-        Adds a single component with 2-to-2 graph_fn to the core and passes two containers through it
-        with flatten/split options enabled.
-        """
-        input1_space = spaces.Dict(a=float, b=spaces.FloatBox(shape=(1, 2)))
-        input2_space = spaces.Dict(a=float, b=float)
+    # TODO new dummies for  this?
 
-        component = FlattenSplitDummy()
-        test = ComponentTest(
-            component=component,
-            input_spaces=dict(
-                in1_fsu=input1_space,
-                in2_fsu=input2_space,
-            )
-        )
-
-        # Options: fsu=flat/split.
-        in1_fsu = dict(a=np.array(0.234), b=np.array([[0.0, 3.0]]))
-        in2_fsu = dict(a=np.array(5.0), b=np.array(5.5))
-        # Result of sending 'a' keys through graph_fn: (in1[a]+1.0=1.234, in1[a]+in2[a]=5.234)
-        # Result of sending 'b' keys through graph_fn: (in1[b]+1.0=[[1, 4]], in1[b]+in2[b]=[[5.5, 8.5]])
-        out1_fsu = dict(a=1.234, b=np.array([[1.0, 4.0]]))
-        out2_fsu = dict(a=np.array(5.234, dtype=np.float32), b=np.array([[5.5, 8.5]]))
-        test.test(out_socket_names=["out1_fsu", "out2_fsu"],
-                  inputs=dict(in1_fsu=in1_fsu, in2_fsu=in2_fsu),
-                  expected_outputs=[out1_fsu, out2_fsu]
-                  )
-
-    def test_1_containers_1_float_flattening_splitting(self):
-        """
-        Adds a single component with 2-to-2 graph_fn to the core and passes one container and one float through it
-        with flatten/split options all disabled.
-        """
-        input1_space = spaces.Dict(a=float, b=spaces.FloatBox(shape=(1, 2)))
-        input2_space = spaces.FloatBox(shape=(1,1))
-
-        component = FlattenSplitDummy()
-        test = ComponentTest(
-            component=component,
-            input_spaces=dict(
-                in1_fsu=input1_space,
-                in2_fsu=input2_space,
-            )
-        )
-
-        # Options: fsu=flat/split.
-        in1_fsu = dict(a=np.array(0.234), b=np.array([[0.0, 3.0]]))
-        in2_fsu = np.array([[2.0]])
-        # Result of sending 'a' keys through graph_fn: (in1[a]+1.0=1.234, in1[a]+in2=2.234)
-        # Result of sending 'b' keys through graph_fn: (in1[b]+1.0=[[1, 4]], in1[b]+in2=[[2.0, 5.0]])
-        out1_fsu = dict(a=1.234, b=np.array([[1.0, 4.0]]))
-        out2_fsu = dict(a=np.array([[2.234]], dtype=np.float32), b=np.array([[2.0, 5.0]]))
-        test.test(out_socket_names=["out1_fsu", "out2_fsu"],
-                  inputs=dict(in1_fsu=in1_fsu, in2_fsu=in2_fsu),
-                  expected_outputs=[out1_fsu, out2_fsu]
-                  )
-
-    def test_2_containers_no_options(self):
-        """
-        Adds a single component with 2-to-2 graph_fn to the core and passes one container and one float through it
-        with no flatten/split options enabled.
-        """
-        input1_space = spaces.Dict(a=int, b=bool)
-        input2_space = spaces.Dict(c=bool, d=int)
-
-        component = NoFlattenNoSplitDummy()
-        test = ComponentTest(
-            component=component,
-            input_spaces=dict(
-                in1=input1_space,
-                in2=input2_space,
-            )
-        )
-
-        # Options: fsu=flat/split.
-        in1 = dict(a=5, b=True)
-        in2 = dict(c=False, d=3)
-        # Expect reversal (see graph_fn)
-        out1 = in2
-        out2 = in1
-        test.test(out_socket_names=["out1", "out2"],
-                  inputs=dict(in1=in1, in2=in2),
-                  expected_outputs=[out1, out2]
-                  )
-
-    def test_1_container_1_float_only_flatten(self):
-        """
-        Adds a single component with 2-to-3 graph_fn to the core and passes one container and one float through it
-        with only the flatten option enabled.
-        """
-        input1_space = spaces.Dict(a=float, b=float, c=spaces.Tuple(float))
-        input2_space = spaces.FloatBox(shape=(1,))
-
-        component = OnlyFlattenDummy(constant_value=5.0)
-        test = ComponentTest(
-            component=component,
-            input_spaces=dict(
-                in1=input1_space,
-                in2=input2_space,
-            )
-        )
-
-        # Options: only flatten_ops=True.
-        in1 = dict(a=5.4, b=3.4, c=tuple([3.2]))
-        in2 = np.array([1.2])
-        # out1: dict(in1_f key: in1_f value + in2_f[""])
-        # out2: in2_f
-        # out3: self.constant_value
-        out1 = dict(a=in1["a"] + in2, b=in1["b"] + in2, c=tuple([in1["c"][0] + in2]))
-        out2 = dict(a=in1["a"] - in2, b=in1["b"] - in2, c=tuple([in1["c"][0] - in2]))
-        out3 = in2
-        test.test(out_socket_names=["out1", "out2", "out3"],
-                  inputs=dict(in1=in1, in2=in2),
-                  expected_outputs=[out1, out2, out3],
-                  decimals=5
-                  )
+    # def test_2_containers_flattening_splitting(self):
+    #     """
+    #     Adds a single component with 2-to-2 graph_fn to the core and passes two containers through it
+    #     with flatten/split options enabled.
+    #     """
+    #     input1_space = spaces.Dict(a=float, b=spaces.FloatBox(shape=(1, 2)))
+    #     input2_space = spaces.Dict(a=float, b=float)
+    #
+    #     component = FlattenSplitDummy()
+    #     test = ComponentTest(
+    #         component=component,
+    #         input_spaces=dict(
+    #             in1_fsu=input1_space,
+    #             in2_fsu=input2_space,
+    #         )
+    #     )
+    #
+    #     # Options: fsu=flat/split.
+    #     in1_fsu = dict(a=np.array(0.234), b=np.array([[0.0, 3.0]]))
+    #     in2_fsu = dict(a=np.array(5.0), b=np.array(5.5))
+    #     # Result of sending 'a' keys through graph_fn: (in1[a]+1.0=1.234, in1[a]+in2[a]=5.234)
+    #     # Result of sending 'b' keys through graph_fn: (in1[b]+1.0=[[1, 4]], in1[b]+in2[b]=[[5.5, 8.5]])
+    #     out1_fsu = dict(a=1.234, b=np.array([[1.0, 4.0]]))
+    #     out2_fsu = dict(a=np.array(5.234, dtype=np.float32), b=np.array([[5.5, 8.5]]))
+    #     test.test(api_method=["out1_fsu", "out2_fsu"],
+    #               params=dict(in1_fsu=in1_fsu, in2_fsu=in2_fsu),
+    #               expected_outputs=[out1_fsu, out2_fsu]
+    #               )
+    #
+    # def test_1_containers_1_float_flattening_splitting(self):
+    #     """
+    #     Adds a single component with 2-to-2 graph_fn to the core and passes one container and one float through it
+    #     with flatten/split options all disabled.
+    #     """
+    #     input1_space = spaces.Dict(a=float, b=spaces.FloatBox(shape=(1, 2)))
+    #     input2_space = spaces.FloatBox(shape=(1,1))
+    #
+    #     component = FlattenSplitDummy()
+    #     test = ComponentTest(
+    #         component=component,
+    #         input_spaces=dict(
+    #             in1_fsu=input1_space,
+    #             in2_fsu=input2_space,
+    #         )
+    #     )
+    #
+    #     # Options: fsu=flat/split.
+    #     in1_fsu = dict(a=np.array(0.234), b=np.array([[0.0, 3.0]]))
+    #     in2_fsu = np.array([[2.0]])
+    #     # Result of sending 'a' keys through graph_fn: (in1[a]+1.0=1.234, in1[a]+in2=2.234)
+    #     # Result of sending 'b' keys through graph_fn: (in1[b]+1.0=[[1, 4]], in1[b]+in2=[[2.0, 5.0]])
+    #     out1_fsu = dict(a=1.234, b=np.array([[1.0, 4.0]]))
+    #     out2_fsu = dict(a=np.array([[2.234]], dtype=np.float32), b=np.array([[2.0, 5.0]]))
+    #     test.test(api_method=["out1_fsu", "out2_fsu"],
+    #               params=dict(in1_fsu=in1_fsu, in2_fsu=in2_fsu),
+    #               expected_outputs=[out1_fsu, out2_fsu]
+    #               )
+    #
+    # def test_2_containers_no_options(self):
+    #     """
+    #     Adds a single component with 2-to-2 graph_fn to the core and passes one container and one float through it
+    #     with no flatten/split options enabled.
+    #     """
+    #     input1_space = spaces.Dict(a=int, b=bool)
+    #     input2_space = spaces.Dict(c=bool, d=int)
+    #
+    #     component = NoFlattenNoSplitDummy()
+    #     test = ComponentTest(
+    #         component=component,
+    #         input_spaces=dict(
+    #             in1=input1_space,
+    #             in2=input2_space,
+    #         )
+    #     )
+    #
+    #     # Options: fsu=flat/split.
+    #     in1 = dict(a=5, b=True)
+    #     in2 = dict(c=False, d=3)
+    #     # Expect reversal (see graph_fn)
+    #     out1 = in2
+    #     out2 = in1
+    #     test.test(api_method=["out1", "out2"],
+    #               params=dict(in1=in1, in2=in2),
+    #               expected_outputs=[out1, out2]
+    #               )
+    #
+    # def test_1_container_1_float_only_flatten(self):
+    #     """
+    #     Adds a single component with 2-to-3 graph_fn to the core and passes one container and one float through it
+    #     with only the flatten option enabled.
+    #     """
+    #     input1_space = spaces.Dict(a=float, b=float, c=spaces.Tuple(float))
+    #     input2_space = spaces.FloatBox(shape=(1,))
+    #
+    #     component = OnlyFlattenDummy(constant_value=5.0)
+    #     test = ComponentTest(
+    #         component=component,
+    #         input_spaces=dict(
+    #             in1=input1_space,
+    #             in2=input2_space,
+    #         )
+    #     )
+    #
+    #     # Options: only flatten_ops=True.
+    #     in1 = dict(a=5.4, b=3.4, c=tuple([3.2]))
+    #     in2 = np.array([1.2])
+    #     # out1: dict(in1_f key: in1_f value + in2_f[""])
+    #     # out2: in2_f
+    #     # out3: self.constant_value
+    #     out1 = dict(a=in1["a"] + in2, b=in1["b"] + in2, c=tuple([in1["c"][0] + in2]))
+    #     out2 = dict(a=in1["a"] - in2, b=in1["b"] - in2, c=tuple([in1["c"][0] - in2]))
+    #     out3 = in2
+    #     test.test(api_method=["out1", "out2", "out3"],
+    #               params=dict(in1=in1, in2=in2),
+    #               expected_outputs=[out1, out2, out3],
+    #               decimals=5
+    #               )
--- yarl/tests/test_core/test_single_sub_components.py
+++ yarl/tests/test_core/test_single_sub_components.py
@@ -23,7 +23,7 @@ import unittest
 
 from yarl.tests import ComponentTest
 from yarl.utils import root_logger
-from yarl.tests.dummy_components import Dummy1to1, Dummy2to1, Dummy1to2, Dummy0to1, Dummy2to1Where1ConnectedWithConstant
+from yarl.tests.dummy_components import *
 
 
 class TestSingleSubComponents(unittest.TestCase):
@@ -32,67 +32,69 @@ class TestSingleSubComponents(unittest.TestCase):
     """
     root_logger.setLevel(level=logging.INFO)
 
+    # TODO which one of these do we want to rewrite?
+
     def test_1to1_component(self):
         """
         Adds a single component with 1-to-1 graph_fn to the core and passes a value through it.
         """
-        component = Dummy1to1(scope="dummy")
-        test = ComponentTest(component=component, input_spaces=dict(input=float))
+        component = Dummy1To1(scope="dummy")
+        test = ComponentTest(component=component)
 
         # Expected output: input + 1.0
-        test.test(out_socket_names="output", inputs=1.0, expected_outputs=2.0)
-        test.test(out_socket_names="output", inputs=-5.0, expected_outputs=-4.0)
+        test.test(api_method="run", params=1.0, expected_outputs=2.0)
+        test.test(api_method="run", params=-5.0, expected_outputs=-4.0)
 
     def test_2to1_component(self):
         """
         Adds a single component with 2-to-1 graph_fn to the core and passes 2 values through it.
         """
-        component = Dummy2to1(scope="dummy")
+        component = Dummy2To1(scope="dummy")
         test = ComponentTest(component=component, input_spaces=dict(input1=float, input2=float))
 
         # Expected output: input1 + input2
-        test.test(out_socket_names="output", inputs=dict(input1=1.0, input2=2.9), expected_outputs=3.9)
-        test.test(out_socket_names="output", inputs=dict(input1=4.9, input2=-0.1),
+        test.test(api_method="output", params=dict(input1=1.0, input2=2.9), expected_outputs=3.9)
+        test.test(api_method="output", params=dict(input1=4.9, input2=-0.1),
                   expected_outputs=np.array(4.8, dtype=np.float32))
 
-    def test_1to2_component(self):
-        """
-        Adds a single component with 1-to-2 graph_fn to the core and passes a value through it.
-        """
-        component = Dummy1to2(scope="dummy")
-        test = ComponentTest(component=component, input_spaces=dict(input=float))
+    # def test_1to2_component(self):
+    #     """
+    #     Adds a single component with 1-to-2 graph_fn to the core and passes a value through it.
+    #     """
+    #     component = Dummy1To2(scope="dummy")
+    #     test = ComponentTest(component=component, input_spaces=dict(input=float))
+    #
+    #     # Expected outputs: (input, input+1.0)
+    #     test.test(api_method=["output1", "output2"], params=1.0, expected_outputs=[1.0, 2.0])
+    #     test.test(api_method=["output1", "output2"], params=4.5, expected_outputs=[4.5, 5.5])
 
-        # Expected outputs: (input, input+1.0)
-        test.test(out_socket_names=["output1", "output2"], inputs=1.0, expected_outputs=[1.0, 2.0])
-        test.test(out_socket_names=["output1", "output2"], inputs=4.5, expected_outputs=[4.5, 5.5])
-
-    def test_0to1_component(self):
-        """
-        Adds a single component with 0-to-1 graph_fn to the core and passes a value through it.
-        """
-        component = Dummy0to1(scope="dummy")
-        test = ComponentTest(component=component, input_spaces=dict(input=float))
-
-        # Expected outputs: Always 0.0
-        test.test(out_socket_names=["output"], inputs=None, expected_outputs=[8.0])
+    # def test_0to1_component(self):
+    #     """
+    #     Adds a single component with 0-to-1 graph_fn to the core and passes a value through it.
+    #     """
+    #     component = Dummy0to1(scope="dummy")
+    #     test = ComponentTest(component=component, input_spaces=dict(input=float))
+    #
+    #     # Expected outputs: Always 0.0
+    #     test.test(api_method="run", params=None, expected_outputs=[8.0])
 
     def test_2to1_component_with_constant_input_value(self):
         """
         Adds a single component with 1-to-1 graph_fn to the core and blocks the input with a constant value.
         """
-        component = Dummy2to1(scope="dummy")
+        component = Dummy2To1(scope="dummy")
         test = ComponentTest(component=component, input_spaces=dict(input1=5.5, input2=float))
 
         # Expected output: (const 5.5) + in2
-        test.test(out_socket_names="output", inputs=dict(input2=4.5), expected_outputs=10.0)
+        test.test(api_method="output", params=dict(input2=4.5), expected_outputs=10.0)
 
-    def test_2to1_component_with_constant_graph_fn_input1(self):
-        """
-        Adds a single component with 2-to-1 graph_fn to the core, and the second input to the
-        graph_fn is already blocked by the component.
-        """
-        component = Dummy2to1Where1ConnectedWithConstant(scope="dummy")
-        test = ComponentTest(component=component, input_spaces=dict(input1=float))
-
-        # Expected output: in1 + (const 3.0)
-        test.test(out_socket_names="output", inputs=dict(input1=4.5), expected_outputs=7.5)
+    # def test_2to1_component_with_constant_graph_fn_input1(self):
+    #     """
+    #     Adds a single component with 2-to-1 graph_fn to the core, and the second input to the
+    #     graph_fn is already blocked by the component.
+    #     """
+    #     component = Dummy2to1Where1ConnectedWithConstant(scope="dummy")
+    #     test = ComponentTest(component=component, input_spaces=dict(input1=float))
+    #
+    #     # Expected output: in1 + (const 3.0)
+    #     test.test(api_method="output", params=dict(input1=4.5), expected_outputs=7.5)
