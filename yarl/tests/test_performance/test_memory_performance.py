4ba37a97d90e4ced7c1a0a6e35bae30d7ebfab39
Michael Schaarschmidt <mi.schaarschmidt@gmail.com>
2018-07-07 11:23:16 +0100

    Adding sample performance test.

--- yarl/tests/test_performance/test_memory_performance.py
+++ yarl/tests/test_performance/test_memory_performance.py
@@ -21,7 +21,7 @@ import unittest
 
 import time
 
-from yarl.components import ReplayMemory
+from yarl.components import ReplayMemory, PrioritizedReplay
 from yarl.envs import OpenAIGymEnv
 from yarl.spaces import Dict, BoolBox
 from yarl.tests import ComponentTest
@@ -33,9 +33,18 @@ class TestMemoryPerformance(unittest.TestCase):
     # large amount sof memory.
     env = OpenAIGymEnv(gym_env='CartPole-v0')
 
+    # Inserts.
     capacity = 100000
     inserts = 1000
     chunk_size = 64
+
+    # Samples.
+    samples = 1000
+    sample_batch_size = 64
+
+    alpha = 1.0
+    beta = 1.0
+    max_priority = 1.0
 
     def test_replay_insert(self):
         """
@@ -64,9 +73,11 @@ class TestMemoryPerformance(unittest.TestCase):
         start = time.monotonic()
         for record in records:
             test.test(api_methods=dict(insert_records=record), expected_outputs=None)
-        end = time.time() - start
+        end = time.monotonic() - start
 
         tp = len(records) / end
+        print('#### Testing Replay memory ####')
+        print('Testing insert performance:')
         print('Inserted {} separate records, throughput: {} ops/s, total time: {} s'.format(
             len(records), tp, end
         ))
@@ -80,4 +91,77 @@ class TestMemoryPerformance(unittest.TestCase):
         tp = len(record_chunks) / end
         print('Inserted {} record chunks of size {}, throughput: {} ops/s, total time: {} s'.format(
             len(record_chunks), self.chunk_size, tp, end
+        ))
+
+        print('Testing sample performance:')
+        start = time.monotonic()
+        for _ in range(self.samples):
+            test.test(api_methods=dict(get_records=self.sample_batch_size), expected_outputs=None)
+        end = time.monotonic() - start
+        tp = self.samples / end
+
+        print('Sampled {} batches of size {}, throughput: {} sample-ops/s, total time: {} s'.format(
+            self.samples, self.sample_batch_size, tp, end
+        ))
+
+
+
+    def test_prioritized_replay_insert(self):
+        """
+        Tests individual and chunked insert performance into replay memory.
+        """
+
+        record_space = Dict(
+            states=self.env.state_space,
+            actions=self.env.action_space,
+            reward=float,
+            terminals=BoolBox(),
+            add_batch_rank=True
+        )
+        input_spaces = dict(
+            insert_records=record_space,
+            get_records=int
+        )
+
+        memory = PrioritizedReplay(
+            capacity=self.capacity,
+            next_states=True,
+            alpha=self.alpha,
+            beta=self.beta
+        )
+        test = ComponentTest(component=memory, input_spaces=input_spaces)
+
+        records = [record_space.sample(size=1) for _ in range(self.inserts)]
+        start = time.monotonic()
+        for record in records:
+            test.test(api_methods=dict(insert_records=record), expected_outputs=None)
+        end = time.monotonic() - start
+
+        tp = len(records) / end
+        print('#### Testing Prioritized Replay memory ####')
+        print('Testing insert performance:')
+        print('Inserted {} separate records, throughput: {} ops/s, total time: {} s'.format(
+            len(records), tp, end
+        ))
+
+        record_chunks = [record_space.sample(size=self.chunk_size) for _ in range(self.inserts)]
+        start = time.monotonic()
+        for chunk in record_chunks:
+            test.test(api_methods=dict(insert_records=chunk), expected_outputs=None)
+        end = time.monotonic() - start
+
+        tp = len(record_chunks) * self.chunk_size / end
+        print('Inserted {} record chunks of size {}, throughput: {} ops/s, total time: {} s'.format(
+            len(record_chunks), self.chunk_size, tp, end
+        ))
+
+        print('Testing sample performance:')
+        start = time.monotonic()
+        for _ in range(self.samples):
+            test.test(api_methods=dict(get_records=self.sample_batch_size), expected_outputs=None)
+        end = time.monotonic() - start
+        tp = self.samples / end
+
+        print('Sampled {} batches of size {}, throughput: {} sample-ops/s, total time: {} s'.format(
+            self.samples, self.sample_batch_size, tp, end
         ))
