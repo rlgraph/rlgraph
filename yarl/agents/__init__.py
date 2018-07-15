ceef2d4b5deb9a936a8163aea6962b5fe49a8a46
Michael Schaarschmidt <mi.schaarschmidt@gmail.com>
2018-07-15 14:36:57 +0100

    Fix cyclic important.

--- yarl/agents/__init__.py
+++ yarl/agents/__init__.py
@@ -18,8 +18,8 @@ from __future__ import division
 from __future__ import print_function
 
 from yarl.agents.agent import Agent
-from yarl.agents.apex_agent import ApexAgent
 from yarl.agents.dqn_agent import DQNAgent
+from yarl.agents.apex_agent import ApexAgent
 from yarl.agents.ppo_agent import PPOAgent
 from yarl.agents.random_agent import RandomAgent
 
