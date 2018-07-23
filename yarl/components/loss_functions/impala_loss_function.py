1142d649e5dbc6c7f295336b72128a8cba4bce55
Sven <svenmika1977@gmail.com>
2018-07-23 13:50:47 -0400

    IMPALA LossFunction stub.

--- yarl/components/loss_functions/impala_loss_function.py
+++ yarl/components/loss_functions/impala_loss_function.py
@@ -17,3 +17,36 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+from yarl.components.helpers.v_trace_function import VTraceFunction
+from yarl.components.loss_functions import LossFunction
+
+
+class IMPALALossFunction(LossFunction):
+    """
+    The IMPALA loss function based on v-trace off-policy policy gradient corrections, described in detail in [1].
+
+    [1] IMPALA: Scalable Distributed Deep-RL with Importance Weighted Actor-Learner Architectures - Espeholt, Soyer,
+        Munos et al. - 2018 (https://arxiv.org/abs/1802.01561)
+    """
+    def __init__(self, v_trace_spec=None, **kwargs):
+        super(IMPALALossFunction, self).__init__(scope=kwargs.pop("scope", "impala-loss-func"), **kwargs)
+
+        self.v_trace = VTraceFunction.from_spec(v_trace_spec)
+
+    def _graph_fn_loss_per_item(self, logits, advantages, actions):
+        """
+        Calculates the loss per batch item (summed over all timesteps) based on: Advantages, raw policy logits
+        and actually selected actions from some policy.
+
+        Args:
+            logits (DataOp): The raw logits (before softmaxing) coming from the policy. Dimensions are:
+                batch x time x data.
+            advantages (DataOp): The advantage values coming from the policy. Dimensions are:
+                batch x time x data.
+            actions (DataOp): The actually taken actions. Dimensions are:
+                batch x time x data.
+
+        Returns:
+            DataOp: The loss values per item in the batch, but summed over all timesteps.
+        """
+        pass
