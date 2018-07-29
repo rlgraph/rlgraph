9b6179297252aafc56a91e68b4870697c1838f5a
Sven <svenmika1977@gmail.com>
2018-07-29 09:22:54 -0400

    Conditional import of deepmindlab in Environment __init__ (envs that don't have deepmind lab installed).

--- rlgraph/environments/__init__.py
+++ rlgraph/environments/__init__.py
@@ -18,15 +18,12 @@ from __future__ import division
 from __future__ import print_function
 
 from rlgraph.environments.environment import Environment
-from rlgraph.environments.deepmind_lab import DeepmindLabEnv
 from rlgraph.environments.grid_world import GridWorld
 from rlgraph.environments.openai_gym import OpenAIGymEnv
 from rlgraph.environments.random_env import RandomEnv
 
 
 Environment.__lookup_classes__ = dict(
-    deepmindlab=DeepmindLabEnv,
-    deepmindlabenv=DeepmindLabEnv,
     gridworld=GridWorld,
     openai=OpenAIGymEnv,
     openaigymenv=OpenAIGymEnv,
@@ -34,6 +31,15 @@ Environment.__lookup_classes__ = dict(
     random = RandomEnv,
     randomenv=RandomEnv
 )
+
+try:
+    from rlgraph.environments.deepmind_lab import DeepmindLabEnv
+    Environment.__lookup_classes__.update(dict(
+        deepmindlab=DeepmindLabEnv,
+        deepmindlabenv=DeepmindLabEnv,
+    ))
+except ModuleNotFoundError:
+    pass
 
 __all__ = ["Environment"] + \
           list(set(map(lambda x: x.__name__, Environment.__lookup_classes__.values())))
