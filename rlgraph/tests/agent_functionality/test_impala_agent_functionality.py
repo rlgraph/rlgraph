644aa3d74473ad724abf47273d2870c7a409d321
Michael Schaarschmidt <mi.schaarschmidt@gmail.com>
2018-08-06 08:21:14 +0100

    Move test config.

--- rlgraph/tests/agent_functionality/test_impala_agent_functionality.py
+++ rlgraph/tests/agent_functionality/test_impala_agent_functionality.py
@@ -39,7 +39,7 @@ class TestIMPALAAgentFunctionality(unittest.TestCase):
         """
         env = RandomEnv(state_space=spaces.FloatBox(shape=(2,)), action_space=spaces.IntBox(2), deterministic=True)
         agent = Agent.from_spec(
-            "configs/impala_agent_for_random_env.json",
+            "../configs/impala_agent_for_random_env.json",
             state_space=env.state_space,
             action_space=env.action_space
         )
