504a1d96e4f1f5a6c34e5c3479cf9646683f9689
Michael Schaarschmidt <mi.schaarschmidt@gmail.com>
2018-08-06 10:18:10 +0100

    Fix circular import.

--- rlgraph/components/layers/preprocessing/__init__.py
+++ rlgraph/components/layers/preprocessing/__init__.py
@@ -17,13 +17,13 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from rlgraph.components.layers.preprocessing.convert_type import ConvertType
 from rlgraph.components.layers.preprocessing.preprocess_layer import PreprocessLayer
 
 from rlgraph.components.layers.preprocessing.clip import Clip
 from rlgraph.components.layers.preprocessing.flatten import Flatten
 from rlgraph.components.layers.preprocessing.grayscale import GrayScale
 from rlgraph.components.layers.preprocessing.image_binary import ImageBinary
+from rlgraph.components.layers.preprocessing.convert_type import ConvertType
 from rlgraph.components.layers.preprocessing.image_crop import ImageCrop
 from rlgraph.components.layers.preprocessing.image_resize import ImageResize
 from rlgraph.components.layers.preprocessing.normalize import Normalize
