3ff0024ed03353b30fe7a1cd0df96330c65a9036
Sven <svenmika1977@gmail.com>
2018-08-07 08:13:53 -0400

    Stub files for large IMPALA NeuralNetwork class.

---
+++ rlgraph/components/papers/impala/large_impala_network.py
@@ -0,0 +1,31 @@
+# Copyright 2018 The YARL-Project, All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from rlgraph.components.neural_networks import NeuralNetwork
+
+
+class LargeIMPALANetwork(NeuralNetwork):
+    """
+    The "large architecture" version of the network used in [1].
+
+    [1] IMPALA: Scalable Distributed Deep-RL with Importance Weighted Actor-Learner Architectures - Espeholt, Soyer,
+        Munos et al. - 2018 (https://arxiv.org/abs/1802.01561)
+    """
+    def __init__(self, scope="large-impala-network", **kwargs):
+        super(LargeIMPALANetwork, self).__init__(scope=scope, **kwargs)
