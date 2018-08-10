24b8c5bfd69d63420266a5f2251564f229c68c17
Sven <svenmika1977@gmail.com>
2018-08-09 23:53:22 -0400

    Cont: IMPALA large architecture. - write test case only for large IMPALA Network (within IMPALA functionality test file).

--- rlgraph/tests/agent_functionality/test_impala_agent_functionality.py
+++ rlgraph/tests/agent_functionality/test_impala_agent_functionality.py
@@ -20,24 +20,54 @@ from __future__ import print_function
 import logging
 import unittest
 from rlgraph.agents import Agent
-import rlgraph.spaces as spaces
+from rlgraph.components.papers.impala.large_impala_network import LargeIMPALANetwork
 from rlgraph.environments import RandomEnv
 from rlgraph.execution.single_threaded_worker import SingleThreadedWorker
-from rlgraph.tests import recursive_assert_almost_equal
+from rlgraph.spaces import *
+from rlgraph.tests.component_test import ComponentTest
 from rlgraph.utils import root_logger
 
 
 class TestIMPALAAgentFunctionality(unittest.TestCase):
     """
-    Tests the IMPALAAgent assembly on the RandomEnv.
+    Tests the LargeIMPALANetwork functionality and IMPALAAgent assembly on the RandomEnv.
+    For details on the IMPALA algorithm, see [1]:
+
+    [1] IMPALA: Scalable Distributed Deep-RL with Importance Weighted Actor-Learner Architectures - Espeholt, Soyer,
+        Munos et al. - 2018 (https://arxiv.org/abs/1802.01561)
     """
     root_logger.setLevel(level=logging.INFO)
+
+    def test_large_impala_network_without_agent(self):
+        """
+        Creates a large IMPALA architecture network and runs a few input samples through it.
+        """
+        # Use the exact same input space as in the IMPALA paper.
+        input_space = Dict(
+            image=FloatBox(shape=(96, 72, 3)), text=TextBox(),
+            previous_action=IntBox(), previous_reward=FloatBox(),
+            add_batch_rank=True,
+            add_time_rank=True,
+            time_major=True
+        )
+        action_space = IntBox(9, add_batch_rank=True, add_time_rank=True, time_major=True)
+
+        # Create the network.
+        large_impala_architecture = LargeIMPALANetwork()
+        test = ComponentTest(
+            large_impala_architecture, input_spaces=dict(input_dict=input_space), action_space=action_space
+        )
+
+        # Send a 2x3 sample through the network (2=sequence-length (time-rank), 3=batch-size).
+        sample_input = input_space.sample(size=(2, 3))
+        expected = None
+        test.test(("apply", sample_input), expected_outputs=expected)
 
     def test_impala_assembly(self):
         """
         Creates an IMPALAAgent and runs it for a few steps in the RandomEnv.
         """
-        env = RandomEnv(state_space=spaces.FloatBox(shape=(2,)), action_space=spaces.IntBox(2), deterministic=True)
+        env = RandomEnv(state_space=FloatBox(shape=(2,)), action_space=IntBox(2), deterministic=True)
         agent = Agent.from_spec(
             "../configs/impala_agent_for_random_env.json",
             state_space=env.state_space,
