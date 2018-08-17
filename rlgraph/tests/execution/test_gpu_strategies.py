e9fbb76a770a8b6ab0ebd55de87d0197a2d5f3ce
Michael Schaarschmidt <mi.schaarschmidt@gmail.com>
2018-08-17 20:39:06 +0100

    Prepare multi gpu compilation test case + config.

---
+++ rlgraph/tests/execution/test_gpu_strategies.py
@@ -0,0 +1,43 @@
+# Copyright 2018 The RLgraph authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, softwamre
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import unittest
+from rlgraph.agents import ApexAgent
+from rlgraph.environments import OpenAIGymEnv
+from rlgraph.tests.test_util import config_from_path
+
+
+class TestGpuStrategies(unittest.TestCase):
+    """
+    Tests gpu strategies.
+    """
+    def test_multi_gpu_agent_compilation(self):
+        """
+        Tests if the multi gpu strategy can compile successfully on a multi gpu system.
+
+        THIS TEST REQUIRES A MULTI GPU SYSTEM.
+        """
+        agent_config = config_from_path("configs/multi_gpu_ray_apex_for_pong.json")
+        agent_config["execution_spec"].pop("ray_spec")
+        environment = OpenAIGymEnv("Pong-v0", frameskip=4)
+
+        agent = ApexAgent.from_spec(
+            agent_config, state_space=environment.state_space, action_space=environment.action_space
+        )
+        print('Compiled apex agent')
