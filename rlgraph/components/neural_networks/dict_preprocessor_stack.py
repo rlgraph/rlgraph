9763e34a3e0dcfd859272016ded4b2ad4107c529
sven1977 <svenmika1977@gmail.com>
2018-08-21 05:45:01 +0200

    Add DictPreprocessorStack component to be able to independently preprocess different streams from an environment (which outputs a dict state). Incl. test case.

---
+++ rlgraph/components/neural_networks/dict_preprocessor_stack.py
@@ -0,0 +1,113 @@
+# Copyright 2018 The RLgraph authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from rlgraph import get_backend
+#from rlgraph.components.common.dict_merger import DictMerger
+#from rlgraph.components.common.dict_splitter import DictSplitter
+from rlgraph.components.layers.preprocessing import PreprocessLayer
+from rlgraph.components.neural_networks.preprocessor_stack import PreprocessorStack
+from rlgraph.spaces import ContainerSpace, Dict
+from rlgraph.utils.ops import flatten_op
+from rlgraph.utils.util import default_dict
+
+if get_backend() == "tf":
+    import tensorflow as tf
+
+
+class DictPreprocessorStack(PreprocessorStack):
+    """
+    A generic PreprocessorStack that can handle Dict/Tuple Spaces and parallely preprocess different Spaces within
+    different (and separate) single PreprocessorStack components.
+    The output is again a dict of preprocessed inputs.
+
+    API:
+        preprocess(input_): Outputs the preprocessed input_ after sending it through all sub-Components of this Stack.
+        reset(): An op to trigger all PreprocessorStacks of this Vector to be reset.
+    """
+    def __init__(self, preprocessor_spec, **kwargs):
+        """
+        Args:
+            preprocessor_spec (dict):
+
+        Raises:
+            RLGraphError: If a sub-component is not a PreprocessLayer object.
+        """
+        # Create one separate PreprocessorStack per given key.
+        # All possibly other keys in an input will be pass through un-preprocessed.
+        self.preprocessors = flatten_op(preprocessor_spec)
+        for i, (flat_key, spec) in enumerate(self.preprocessors.items()):
+            self.preprocessors[flat_key] = PreprocessorStack.from_spec(
+                spec, scope="preprocessor-stack-{}".format(i)
+            )
+
+        #self.splitter = DictSplitter(*list(self.preprocessors.keys()))
+        #self.merger = DictMerger(*list(self.preprocessors.keys()))
+
+        # NOTE: No automatic API-methods. Define them all ourselves.
+        kwargs["api_methods"] = {}
+        default_dict(kwargs, dict(scope=kwargs.pop("scope", "dict-preprocessor-stack")))
+        super(DictPreprocessorStack, self).__init__(*list(self.preprocessors.values()), **kwargs)
+
+        self.define_api_method(
+            "preprocess", self._graph_fn_preprocess, flatten_ops=True, split_ops=True, add_auto_key_as_first_param=True,
+            ok_to_overwrite=True
+        )
+
+    def _graph_fn_preprocess(self, key, inputs):
+        # Is a PreprocessorStack defined for this key?
+        if key in self.preprocessors:
+            return self.call(self.preprocessors[key].preprocess, inputs)
+        # Simple pass through, no preprocessing.
+        else:
+            return tf.identity(inputs)
+
+    def reset(self):
+        # TODO: python-Components: For now, we call each preprocessor's graph_fn directly.
+        if self.backend == "python" or get_backend() == "python":
+            for preprocessor in self.preprocessors.values():  # type: PreprocessLayer
+                preprocessor.reset()
+
+        elif get_backend() == "tf":
+            # Connect each pre-processor's "reset" output op via our graph_fn into one op.
+            resets = list()
+            for preprocessor in self.preprocessors.values():  # type: PreprocessorStack
+                resets.append(self.call(preprocessor.reset))
+            reset_op = self.call(self._graph_fn_reset, *resets)
+            return reset_op
+
+    def _graph_fn_reset(self, *preprocessor_resets):
+        if get_backend() == "tf":
+            with tf.control_dependencies(preprocessor_resets):
+                return tf.no_op()
+
+    def get_preprocessed_space(self, space):
+        """
+        Returns the Space obtained after pushing the input through all layers of this Stack.
+
+        Args:
+            space (Space): The incoming Space object.
+
+        Returns:
+            Space: The Space after preprocessing.
+        """
+        assert isinstance(space, ContainerSpace)
+        dict_spec = dict()
+        for pp in self.sub_components.values():
+            space = pp.get_preprocessed_space(space)
+        return Dict(dict_spec)
---
+++ rlgraph/tests/components/test_dict_preprocessor_stack.py
@@ -0,0 +1,57 @@
+# Copyright 2018 The RLgraph authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from copy import deepcopy
+import numpy as np
+from six.moves import xrange as range_
+import unittest
+
+from rlgraph.components.neural_networks.dict_preprocessor_stack import DictPreprocessorStack
+from rlgraph.spaces import *
+from rlgraph.tests import ComponentTest, recursive_assert_almost_equal
+from rlgraph.tests.test_util import config_from_path
+
+
+class TestDictPreprocessorStacks(unittest.TestCase):
+    """
+    Tests dict preprocessor stacks.
+    """
+    def test_dict_preprocessor_stack(self):
+        """
+        Tests if Python and TensorFlow backend return the same output
+        for a standard DQN-style preprocessing stack.
+        """
+        input_space = Dict(
+            a=FloatBox(shape=(2, 3)),
+            b=IntBox(3),
+            add_batch_rank=True
+        )
+        preprocessor_spec = dict(
+            a=[dict(type="divide", divisor=2), dict(type="multiply", factor=4)]
+        )
+
+        dict_preprocessor_stack = DictPreprocessorStack(preprocessor_spec)
+
+        test = ComponentTest(component=dict_preprocessor_stack, input_spaces=dict(inputs=input_space))
+
+        # Run the test.
+        inputs = input_space.sample(5)
+        expected = dict(a=inputs["a"] * 2, b=inputs["b"])
+        test.test("reset")
+        test.test(("preprocess", inputs), expected_outputs=expected)
