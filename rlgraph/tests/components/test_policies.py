129ac0c08194581ba1c62582889fc78cf0c78fc0
sven1977 <svenmika1977@gmail.com>
2018-08-25 13:42:18 +0200

    Fix broken test case for policy using new dueling-action-adapter.

--- rlgraph/tests/components/test_policies.py
+++ rlgraph/tests/components/test_policies.py
@@ -85,7 +85,7 @@ class TestPolicies(unittest.TestCase):
 
     def test_policy_for_discrete_action_space_with_dueling_layer(self):
         # state_space (NN is a simple single fc-layer relu network (2 units), random biases, random weights).
-        state_space = FloatBox(shape=(3,), add_batch_rank=True)
+        nn_input_space = FloatBox(shape=(3,), add_batch_rank=True)
 
         # action_space (2 possible actions).
         action_space = IntBox(2, add_batch_rank=True)
@@ -93,69 +93,74 @@ class TestPolicies(unittest.TestCase):
         # Policy with additional dueling layer.
         policy = Policy(
             neural_network=config_from_path("configs/test_lrelu_nn.json"),
-            action_adapter_spec=dict(type="dueling_action_adapter", action_space=action_space),
-            switched_off_apis={"get_q_values"}
+            action_adapter_spec=dict(
+                type="dueling-action-adapter", action_space=action_space,
+                units_state_value_stream=10,
+                units_advantage_stream=10
+            ),
+            #switched_off_apis={"get_q_values"}
         )
         test = ComponentTest(
             component=policy,
-            input_spaces=dict(nn_input=state_space),
+            input_spaces=dict(nn_input=nn_input_space),
             action_space=action_space
         )
         policy_params = test.read_variable_values(policy.variables)
 
         # Some NN inputs (3 input nodes, batch size=3).
-        states = np.array([[-0.01, 0.02, -0.03], [0.04, -0.05, 0.06], [-0.07, 0.08, -0.09]])
+        nn_input = nn_input_space.sample(size=3)
         # Raw NN-output (3 hidden nodes). All weights=1.5, no biases.
-        expected_nn_output = np.matmul(states, policy_params["policy/test-network/hidden-layer/dense/kernel"])
-        expected_nn_output = relu(expected_nn_output, 0.1)
-        test.test(("get_nn_output", states), expected_outputs=expected_nn_output)
+        expected_nn_output = relu(np.matmul(nn_input, policy_params["policy/test-network/hidden-layer/dense/kernel"]),
+                                  0.1)
+        test.test(("get_nn_output", nn_input), expected_outputs=expected_nn_output)
 
         # Raw action layer output; Expected shape=(3,3): 3=batch, 2=action categories + 1 state value
-        expected_action_layer_output = np.matmul(
-            expected_nn_output, policy_params["policy/dueling-action-adapter/action-layer/dense/kernel"]
+        expected_state_value = np.matmul(np.matmul(
+            expected_nn_output,
+            policy_params["policy/dueling-action-adapter/dense-layer-state-value-stream/dense/kernel"]
+        ), policy_params["policy/dueling-action-adapter/state-value-node/dense/kernel"])
+        expected_raw_advantages = np.matmul(np.matmul(
+            expected_nn_output, policy_params["policy/dueling-action-adapter/dense-layer-advantage-stream/dense/kernel"]
+        ), policy_params["policy/dueling-action-adapter/action-layer/dense/kernel"])
+        test.test(
+            ("get_action_layer_output", nn_input), expected_outputs=(expected_state_value, expected_raw_advantages),
+            decimals=5
         )
-        expected_action_layer_output = np.reshape(expected_action_layer_output, newshape=(3, 3))
-        test.test(("get_action_layer_output", states), expected_outputs=expected_action_layer_output)
 
         # State-values: One for each item in the batch (simply take first out-node of action_layer).
-        expected_state_value_output = np.squeeze(expected_action_layer_output[:, :1], axis=-1)
+        #expected_state_value_output = np.squeeze(expected_action_layer_output[:, :1], axis=-1)
         # Advantage-values: One for each action-choice per item in the batch (simply take second and third out-node
-        expected_advantage_values_output = expected_action_layer_output[:, 1:]
+        #expected_advantage_values_output = expected_action_layer_output[:, 1:]
         # Q-values: One for each action-choice per item in the batch (calculate from state-values and advantage-values
-        expected_q_values_output = np.reshape(expected_state_value_output, newshape=(3, 1)) + \
-                                   expected_advantage_values_output - \
-                                   np.mean(expected_advantage_values_output, axis=-1, keepdims=True)
-        test.test(("get_dueling_output", states), expected_outputs=[
-            expected_state_value_output,
-            expected_advantage_values_output,
-            expected_q_values_output
-        ])
+        expected_q_values_output = expected_state_value + expected_raw_advantages - \
+            np.mean(expected_raw_advantages, axis=-1, keepdims=True)
+        test.test(("get_q_values", nn_input), expected_outputs=expected_q_values_output, decimals=5)
 
         expected_actions = np.argmax(expected_q_values_output, axis=-1)
-        test.test(("get_action", states), expected_outputs=expected_actions)
+        test.test(("get_action", nn_input), expected_outputs=expected_actions)
 
         # Parameter (probabilities). Softmaxed q_values.
         expected_probabilities_output = softmax(expected_q_values_output, axis=-1)
-        test.test(("get_logits_parameters_log_probs", states, [0, 1]),
+        test.test(("get_logits_parameters_log_probs", nn_input, [0, 1]),
                   expected_outputs=[
                       expected_q_values_output,
                       expected_probabilities_output
                       #np.log(expected_probabilities_output),
-                  ])
+                  ], decimals=5)
 
         print("Probs: {}".format(expected_probabilities_output))
 
         # Stochastic sample.
-        expected_actions = np.array([0, 0, 1])
-        test.test(("get_stochastic_action", states), expected_outputs=expected_actions)
+        expected_actions = np.array([0, 0, 0])
+        test.test(("get_stochastic_action", nn_input), expected_outputs=expected_actions)
 
         # Deterministic sample.
-        expected_actions = np.array([1, 0, 1])
-        test.test(("get_max_likelihood_action", states), expected_outputs=expected_actions)
+        expected_actions = np.array([0, 0, 0])
+        test.test(("get_max_likelihood_action", nn_input), expected_outputs=expected_actions)
 
         # Distribution's entropy.
-        expected_h = np.array([0.6931462, 0.6925241, 0.6931312])
-        test.test(("get_entropy", states), expected_outputs=expected_h, decimals=3)
+        expected_h = np.array([0.587, 0.468, 0.588])
+        test.test(("get_entropy", nn_input), expected_outputs=expected_h, decimals=3)
 
     def test_policy_for_discrete_action_space_with_baseline_layer(self):
         # state_space (NN is a simple single fc-layer relu network (2 units), random biases, random weights).
