b6b0131a4d26730b00e591b22e0bdd1c84292ce0
sven1977 <svenmika1977@gmail.com>
2018-08-29 14:00:58 +0200

    Add discount calculation to IMPALA loss function.

--- rlgraph/components/loss_functions/impala_loss_function.py
+++ rlgraph/components/loss_functions/impala_loss_function.py
@@ -42,9 +42,10 @@ class IMPALALossFunction(LossFunction):
     [1] IMPALA: Scalable Distributed Deep-RL with Importance Weighted Actor-Learner Architectures - Espeholt, Soyer,
         Munos et al. - 2018 (https://arxiv.org/abs/1802.01561)
     """
-    def __init__(self, weight_pg=None, weight_baseline=None, weight_entropy=None, **kwargs):
+    def __init__(self, discount=0.99, weight_pg=None, weight_baseline=None, weight_entropy=None, **kwargs):
         """
         Args:
+            discount (float): The discount factor (gamma) to use.
             weight_pg (float): The coefficient used for the policy gradient loss term (L[PG]).
             weight_baseline (float): The coefficient used for the Value-function baseline term (L[V]).
             weight_entropy (float): The coefficient used for the entropy regularization term (L[E]).
@@ -52,6 +53,7 @@ class IMPALALossFunction(LossFunction):
         """
         super(IMPALALossFunction, self).__init__(scope=kwargs.pop("scope", "impala-loss-func"), **kwargs)
 
+        self.discount = discount
         self.v_trace_function = VTraceFunction()  # type: VTraceFunction
 
         self.weight_pg = weight_pg if weight_pg is not None else 1.0
@@ -109,12 +111,15 @@ class IMPALALossFunction(LossFunction):
         log_probs_actions_taken_pi = tf.reduce_sum(input_tensor=(log_probs_actions_pi * one_hot), axis=-1)
         log_is_weights = log_probs_actions_taken_pi - log_probs_actions_taken_mu
 
+        # Discounts are simply 0.0, if there is a terminal, otherwise: `discount`.
+        discounts = tf.to_float(~terminals) * self.discount
+
         # Let the v-trace  helper function calculate the v-trace values (vs) and the pg-advantages
         # (already multiplied by rho_t_pg): A = rho_t_pg * (rt + gamma*vt - V(t)).
         # Both vs and pg_advantages will block the gradient as they should be treated as constants by the gradient
         # calculator of this loss func.
-        vs, pg_advantages = self.call(self.v_trace_function.calc_v_trace_values,
-            log_is_weights, discounts, rewards, values, bootstrapped_values
+        vs, pg_advantages = self.call(
+            self.v_trace_function.calc_v_trace_values, log_is_weights, discounts, rewards, values, bootstrapped_values
         )
 
         if get_backend() == "tf":
