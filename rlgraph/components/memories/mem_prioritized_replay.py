d67272516c24010c5e579bdfc73e91654d1219ec
Michael Schaarschmidt <mi.schaarschmidt@gmail.com>
2018-09-03 20:21:19 +0100

    Space checks for pytorch op builds

--- rlgraph/components/memories/mem_prioritized_replay.py
+++ rlgraph/components/memories/mem_prioritized_replay.py
@@ -20,6 +20,7 @@ from __future__ import print_function
 import numpy as np
 import operator
 
+from rlgraph.utils import SMALL_NUMBER
 from six.moves import xrange as range_
 
 from rlgraph.components import Memory
@@ -90,7 +91,9 @@ class MemPrioritizedReplay(Memory):
             # as this would cause extra memory overhead.
             self.flat_state_keys = [k for k in self.record_registry.keys() if k[:7] == "states-"]
 
-    def _graph_fn_insert_records(self, records):
+    def _graph_fn_insert_records(self, records=None):
+        if records is None or len(records['/reward'][0]) == 0:
+            return
         num_records = len(records[self.fixed_key])
 
         if num_records == 1:
@@ -178,35 +181,36 @@ class MemPrioritizedReplay(Memory):
              dict: Record value dict.
         """
         records = dict()
-        for name in self.record_registry.keys():
-            records[name] = []
-        if self.next_states:
-            for flat_state_key in self.flat_state_keys:
-                flat_next_state_key = "next_states" + flat_state_key[len("states"):]
-                records[flat_next_state_key] = []
-
-        for index in indices:
-            record = self.memory_values[index]
+        if self.size > 0:
             for name in self.record_registry.keys():
-                records[name].append(record[name])
-
+                records[name] = []
             if self.next_states:
-                # TODO these are largely copies
-                next_index = (index + 1) % self.capacity
                 for flat_state_key in self.flat_state_keys:
-                    next_record = self.memory_values[next_index]
-                    flat_next_state_key = "next_states"+flat_state_key[len("states"):]
-                    records[flat_next_state_key].append(next_record[flat_state_key])
+                    flat_next_state_key = "next_states" + flat_state_key[len("states"):]
+                    records[flat_next_state_key] = []
+
+            for index in indices:
+                record = self.memory_values[index]
+                for name in self.record_registry.keys():
+                    records[name].append(record[name])
+
+                if self.next_states:
+                    # TODO these are largely copies
+                    next_index = (index + 1) % self.capacity
+                    for flat_state_key in self.flat_state_keys:
+                        next_record = self.memory_values[next_index]
+                        flat_next_state_key = "next_states"+flat_state_key[len("states"):]
+                        records[flat_next_state_key].append(next_record[flat_state_key])
         return records
 
-    def _graph_fn_get_records(self, num_records):
+    def _graph_fn_get_records(self, num_records=1):
         indices = []
         prob_sum = self.merged_segment_tree.sum_segment_tree.get_sum(0, self.size - 1)
         samples = np.random.random(size=(num_records,)) * prob_sum
         for sample in samples:
             indices.append(self.merged_segment_tree.sum_segment_tree.index_of_prefixsum(prefix_sum=sample))
 
-        sum_prob = self.merged_segment_tree.sum_segment_tree.get_sum()
+        sum_prob = self.merged_segment_tree.sum_segment_tree.get_sum() + SMALL_NUMBER
         min_prob = self.merged_segment_tree.min_segment_tree.get_min_value() / sum_prob
         max_weight = (min_prob * self.size) ** (-self.beta)
         weights = []
@@ -219,8 +223,9 @@ class MemPrioritizedReplay(Memory):
         return self.read_records(indices=indices), indices, np.asarray(weights)
 
     def _graph_fn_update_records(self, indices, update):
-        for index, loss in zip(indices, update):
-            priority = np.power(loss, self.alpha)
-            self.merged_segment_tree.insert(index, priority)
-            self.max_priority = max(self.max_priority, priority)
+        if len(indices) > 0 and indices[0]:
+            for index, loss in zip(indices, update):
+                priority = np.power(loss, self.alpha)
+                self.merged_segment_tree.insert(index, priority)
+                self.max_priority = max(self.max_priority, priority)
 
