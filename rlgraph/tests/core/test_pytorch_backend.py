4a42d6ec4693458a6acf4acfa721e226d51d4052
Michael Schaarschmidt <mi.schaarschmidt@gmail.com>
2018-09-08 14:55:15 +0100

    Create pt test config.

--- rlgraph/tests/core/test_pytorch_backend.py
+++ rlgraph/tests/core/test_pytorch_backend.py
@@ -131,7 +131,7 @@ class TestPytorchBackend(unittest.TestCase):
         Creates a DQNAgent and runs it via a Runner on an openAI Pong Env.
         """
         env = OpenAIGymEnv("Pong-v0", frameskip=4, max_num_noops=30, episodic_life=True)
-        agent_config = config_from_path("configs/dqn_agent_for_pong.json")
+        agent_config = config_from_path("configs/dqn_pytorch_test.json")
         agent = DQNAgent.from_spec(
             # Uses 2015 DQN parameters as closely as possible.
             agent_config,
