371c078dc0f397198e2746c4d755a840093edc86
Michael Schaarschmidt <mi.schaarschmidt@gmail.com>
2018-09-11 10:37:35 +0100

    PyTorch: Add same padded conv2d impl.

--- rlgraph/utils/pytorch_util.py
+++ rlgraph/utils/pytorch_util.py
@@ -128,3 +128,23 @@ def pytorch_reduce_mean(tensor, axes, keepdims):
         for axis in sorted(axes, reverse=True):
             tensor = tensor.mean(axis, keepdim=False)
     return tensor
+
+
+class SamePaddedConv2d(torch.nn.Module):
+    """
+    Implements a Conv2d layer with padding 'same' as PyTorch does not have
+    padding options like TF.
+    """
+    def __init__(self, in_channels, out_channels, kernel_size, bias=True,
+                 padding_layer=torch.nn.ReflectionPad2d):
+        super().__init__()
+        ka = kernel_size // 2
+        kb = ka - 1 if kernel_size % 2 == 0 else ka
+
+        self.net = torch.nn.Sequential(
+            padding_layer((ka,kb,ka,kb)),
+            torch.nn.Conv2d(in_channels, out_channels, kernel_size, bias=bias)
+        )
+
+    def forward(self, x):
+        return self.net(x)
