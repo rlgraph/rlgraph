e1e363eb5c9fc86cd703745c6a36d86791144fff
Michael Schaarschmidt <mi.schaarschmidt@gmail.com>
2018-09-12 17:08:30 +0100

    Use array copy and correct terminal check in dm lab.

--- rlgraph/environments/deepmind_lab.py
+++ rlgraph/environments/deepmind_lab.py
@@ -105,9 +105,9 @@ class DeepmindLabEnv(Environment):
     def step_for_env_stepper(self, actions):
         # Do the actual step.
         reward = self.level.step(action=self.action_list[actions], num_steps=self.frameskip)
-        terminal = not self.level.is_running()
+        terminal = np.array(not self.level.is_running())
         # Flow Env logic.
-        if terminal is True:
+        if terminal:
             state = self.reset()
         else:
             state = self.level.observations()
