be46404960395f6db684c241381fed8dad87f408
Sven <svenmika1977@gmail.com>
2018-09-12 15:42:28 +0200

    Reactivate starting the SpecifiableServer(s) via a threadpool, instead of sequentially from the main thread.

--- rlgraph/utils/specifiable_server.py
+++ rlgraph/utils/specifiable_server.py
@@ -300,27 +300,27 @@ if get_backend() == "tf":
             """
             Starts all registered RLGraphProxyProcess processes.
             """
-            #tp = multiprocessing.pool.ThreadPool()
-            #tp.map(lambda server: server.start(), SpecifiableServer.INSTANCES)
+            tp = multiprocessing.pool.ThreadPool()
+            tp.map(lambda server: server.start(), SpecifiableServer.INSTANCES)
 
-            for server in SpecifiableServer.INSTANCES:
-                server.start()
+            #for server in SpecifiableServer.INSTANCES:
+            #    server.start()
 
             # Erase all SpecifiableServers as we open the Session (after having started all of them),
             # so new ones can get registered.
             self.specifiable_buffer = SpecifiableServer.INSTANCES[:]  # copy list
             SpecifiableServer.INSTANCES.clear()
 
-            #tp.close()
-            #tp.join()
+            tp.close()
+            tp.join()
 
         def end(self, session):
-            #tp = multiprocessing.pool.ThreadPool()
-            #tp.map(lambda server: server.stop(), self.specifiable_buffer)
-            #tp.close()
-            #tp.join()
-            for server in self.specifiable_buffer:
-                server.stop()
+            tp = multiprocessing.pool.ThreadPool()
+            tp.map(lambda server: server.stop(), self.specifiable_buffer)
+            tp.close()
+            tp.join()
+            #for server in self.specifiable_buffer:
+            #    server.stop()
 
 
 
