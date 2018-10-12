952684d78b7e427892ea8da6fb0d5f2c8d7cb9b6
sven1977 <svenmika1977@gmail.com>
2018-10-12 16:05:53 +0200

    Throw away `unify_nn_and_rnn_api_output` utility function as we now solve this problem via dict returns for RNN-based NNs.

--- rlgraph/utils/__init__.py
+++ rlgraph/utils/__init__.py
@@ -22,7 +22,7 @@ from rlgraph.utils.ops import DataOp, SingleDataOp, DataOpDict, DataOpTuple, Con
 from rlgraph.utils.specifiable import Specifiable
 from rlgraph.utils.initializer import Initializer
 from rlgraph.utils.util import dtype, get_shape, get_rank, force_tuple, force_list, LARGE_INTEGER, SMALL_NUMBER, \
-    tf_logger, print_logging_handler, root_logger, logging_formatter, default_dict, unify_nn_and_rnn_api_output
+    tf_logger, print_logging_handler, root_logger, logging_formatter, default_dict
 from rlgraph.utils.numpy import softmax, relu, one_hot
 from rlgraph.utils.pytorch_util import pytorch_one_hot, PyTorchVariable
 from rlgraph.utils.execution_util import print_call_chain
