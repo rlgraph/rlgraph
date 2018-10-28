f733469ef5a0ada5fa009b9c3cdc29ae0b2c5c19
Michael Schaarschmidt <mi.schaarschmidt@gmail.com>
2018-10-28 14:18:03 +0000

    Update GAE/Loss with helper.

--- rlgraph/components/helpers/generalized_advantage_estimation.py
+++ rlgraph/components/helpers/generalized_advantage_estimation.py
@@ -19,10 +19,12 @@ from __future__ import print_function
 
 from rlgraph import get_backend
 from rlgraph.components import Component
+from rlgraph.components.helpers import SequenceHelper
 from rlgraph.utils.decorators import rlgraph_api
 
 if get_backend() == "tf":
     import tensorflow as tf
+    import trfl
 
 
 class GeneralizedAdvantageEstimation(Component):
@@ -41,22 +43,30 @@ class GeneralizedAdvantageEstimation(Component):
             discount (float): Discount gamma.
         """
         super(GeneralizedAdvantageEstimation, self).__init__(device=device, scope=scope, **kwargs)
-
         self.gae_lambda = gae_lambda
         self.discount = discount
 
+        self.sequence_helper = SequenceHelper()
+        self.add_components(self.sequence_helper)
+
     @rlgraph_api(must_be_complete=False)
-    def _graph_fn_calc_gae_values(self, values, rewards):
+    def _graph_fn_calc_gae_values(self, rewards, terminals):
         """
         Returns advantage values based on GAE.
 
         Args:
-            values (DataOp): Value estimates for sample trajectory.
             rewards (DataOp): Rewards in sample trajectory.
+            terminals (DataOp): Termnials in sample trajectory.
 
         Returns:
             PG-advantage values used for training via policy gradient with baseline.
         """
         if get_backend() == "tf":
             gae_discount = self.gae_lambda * self.discount
-            # TODO impl gae discount
+            sequence_lengths = self.sequence_helper.calc_sequence_lengths(terminals)
+
+            # deltas = rewards + self.discount * baseline_values[1:]
+            # TODO call TRFL
+            advantages = None
+
+            return advantages
