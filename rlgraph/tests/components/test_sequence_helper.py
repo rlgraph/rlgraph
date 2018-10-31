d759ed4a4995e3722f4cc98585fa0d0d2945e99a
Michael Schaarschmidt <mi.schaarschmidt@gmail.com>
2018-10-31 20:17:23 +0000

    Add decay+length calc tests for helper.

--- rlgraph/tests/components/test_sequence_helper.py
+++ rlgraph/tests/components/test_sequence_helper.py
@@ -21,7 +21,7 @@ import unittest
 
 from rlgraph.components.helpers.sequence_helper import SequenceHelper
 from rlgraph.spaces import IntBox
-from rlgraph.tests import ComponentTest
+from rlgraph.tests import ComponentTest, recursive_assert_almost_equal
 import numpy as np
 
 
@@ -46,3 +46,36 @@ class TestSequenceHelper(unittest.TestCase):
         input_ = np.asarray([1, 0, 0, 1])
         test.test(("calc_sequence_lengths", input_), expected_outputs=[1, 3])
 
+    def test_calc_decays(self):
+        """
+        Tests counting sequence lengths based on terminal configurations.
+        """
+        sequence_helper = SequenceHelper()
+        decay_value = 0.5
+
+        sequence_indices = dict(
+            sequence_indices=IntBox(add_batch_rank=True),
+            decay=float
+        )
+        test = ComponentTest(component=sequence_helper, input_spaces=sequence_indices)
+        input_ = np.asarray([0, 0, 0, 0])
+        expected_decays = [1.0, 0.5, 0.25, 0.125]
+        lengths, decays = test.test(("calc_sequence_decays", [input_, decay_value]))
+
+        # Check lengths and decays.
+        recursive_assert_almost_equal(x=lengths, y=[4])
+        recursive_assert_almost_equal(x=decays, y=expected_decays)
+
+        input_ = np.asarray([0, 0, 1, 0])
+        expected_decays = [1.0, 0.5, 0.25, 1.0]
+        lengths, decays = test.test(("calc_sequence_decays", [input_, decay_value]))
+
+        recursive_assert_almost_equal(x=lengths, y=[3, 1])
+        recursive_assert_almost_equal(x=decays, y=expected_decays)
+
+        input_ = np.asarray([1, 1, 1, 1])
+        expected_decays = [1.0, 1.0, 1.0, 1.0]
+        lengths, decays = test.test(("calc_sequence_decays", [input_, decay_value]))
+
+        recursive_assert_almost_equal(x=lengths, y=[1, 1, 1, 1])
+        recursive_assert_almost_equal(x=decays, y=expected_decays)
