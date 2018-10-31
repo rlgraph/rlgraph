2c0a0b002b12874c51cffd8b7af9b1f1d1a08e41
Michael Schaarschmidt <mi.schaarschmidt@gmail.com>
2018-10-31 19:54:34 +0000

    Add decay computation utility to sequence helper.

--- rlgraph/components/helpers/sequence_helper.py
+++ rlgraph/components/helpers/sequence_helper.py
@@ -23,6 +23,8 @@ from rlgraph.utils.decorators import rlgraph_api
 
 if get_backend() == "tf":
     import tensorflow as tf
+elif get_backend() == "pytorch":
+    import torch
 
 
 class SequenceHelper(Component):
@@ -80,7 +82,6 @@ class SequenceHelper(Component):
                 parallel_iterations=1,
                 back_prop=False
             )
-            final_length = tf.Print(final_length, [final_length], message="final_length=")
             # If the final element was terminal -> already included.
             sequence_lengths, _, _ = tf.cond(
                 pred=tf.greater(final_length, 0),
@@ -88,7 +89,7 @@ class SequenceHelper(Component):
                 false_fn=lambda: (sequence_lengths, write_index, final_length)
             )
             return sequence_lengths.stack()
-        elif get_backend() == "pytorch" or get_backend() == "python":
+        elif get_backend() == "pytorch":
             sequence_lengths = []
             length = 0
             for index in sequence_indices:
@@ -99,5 +100,104 @@ class SequenceHelper(Component):
             # Append final sequence.
             if length > 0:
                 sequence_lengths.append(length)
-            return sequence_lengths
+            return torch.tensor(sequence_lengths, dtype=torch.int32)
+
+    @rlgraph_api(returns=2)
+    def _graph_fn_calc_sequence_decays(self, sequence_indices, decay):
+        """
+        Computes decays for sequence indices, e.g. for generalized advantage estimation.
+        That is, a sequence with terminals is used to compute for each subsequence the decay
+        values and the length of the sequence.
+
+        Example:
+        decay = 0.5, sequence_indices = [0 0 1 0 1] will return lengths [3, 2] and
+        decays [1 0.5 0.25 1 0.5] (decay^0, decay^1, ..decay^k) where k = sequence length for
+        each sub-sequence.
+
+        Args:
+            sequence_indices (DataOp): Indices denoting sequences, e.g. terminal values.
+            decay (float): Initial decay value to start sub-sequence with.
+
+        Returns:
+            Sequence lengths and their decays.
+        """
+        if get_backend() == "tf":
+            elems = tf.shape(input=sequence_indices)[0]
+            # TensorArray:
+            sequence_lengths = tf.TensorArray(
+                dtype=tf.int32,
+                infer_shape=False,
+                size=1,
+                dynamic_size=True,
+                clear_after_read=False
+            )
+            decays = tf.TensorArray(
+                dtype=tf.float32,
+                infer_shape=False,
+                size=1,
+                dynamic_size=True,
+                clear_after_read=False
+            )
+
+            def update(write_index, sequence_array, length):
+                # Write to index, increase
+                sequence_array = sequence_array.write(write_index, length)
+                return sequence_array, write_index + 1, 0
+
+            def insert_body(index, length, sequence_lengths, write_index, decays):
+                # Decay is based on length, so val = decay^length
+                decay_val = tf.pow(x=decay, y=length)
+
+                # Write decay val into array.
+                decays = decays.write(index, decay_val)
+                length += 1
+
+                # Update tensor array, reset length to 0.
+                sequence_lengths, write_index, length = tf.cond(
+                    pred=tf.equal(sequence_indices[index], 1),
+                    true_fn=lambda: update(write_index, sequence_lengths, length),
+                    false_fn=lambda: (sequence_lengths, write_index, length)
+                )
+                return index + 1, length, sequence_lengths, write_index, decays
+
+            def cond(index, length, sequence_lengths, write_index, decays):
+                return index < elems
+
+            _, final_length, sequence_lengths, write_index = tf.while_loop(
+                cond=cond,
+                body=insert_body,
+                loop_vars=[0, 0, sequence_lengths, 0, decays],
+                parallel_iterations=1,
+                back_prop=False
+            )
+
+            # If the final element was terminal -> already included.
+            # Decays need no updating because we just wrote them always.
+            sequence_lengths, _, _ = tf.cond(
+                pred=tf.greater(final_length, 0),
+                true_fn=lambda: update(write_index, sequence_lengths, final_length),
+                false_fn=lambda: (sequence_lengths, write_index, final_length)
+            )
+
+            return sequence_lengths.stack(), decays.stack()
+        elif get_backend() == "pytorch":
+            sequence_lengths = []
+            decays = []
+
+            length = 0
+            for index in sequence_indices:
+                # Compute decay based on sequence length.
+                decays.append(pow(decay, length))
+                length += 1
+                if index == 1:
+                    sequence_lengths.append(length)
+                    length = 0
+
+            # Append final sequence.
+            if length > 0:
+                sequence_lengths.append(length)
+            return torch.tensor(sequence_lengths, dtype=torch.int32),\
+                   torch.tensor(decays, dtype=torch.int32)
+
+
 
