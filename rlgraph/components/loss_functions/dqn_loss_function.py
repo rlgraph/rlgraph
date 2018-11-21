f86669ae09493328344ea0f75f2c9a9ec3fd6de0
sven1977 <svenmika1977@gmail.com>
2018-11-21 08:42:15 +0100

    DQN Loss func: - for container actions: use a global mean target (same for all sub-actions). - always multiply with 0.5 (not only for Huber loss).

--- rlgraph/components/loss_functions/dqn_loss_function.py
+++ rlgraph/components/loss_functions/dqn_loss_function.py
@@ -50,8 +50,8 @@ class DQNLossFunction(LossFunction):
                  scope="dqn-loss-function", **kwargs):
         """
         Args:
-            double_q (bool): Whether to use the double DQN loss function (see DQNAgent [2]).
-            huber_loss (bool): Whether to apply a huber loss correction(see DQNAgent [2]).
+            double_q (bool): Whether to use the double DQN loss function ([2]).
+            huber_loss (bool): Whether to apply a huber loss correction ([3]).
             importance_weights (bool): Where to use importance weights from a prioritized replay.
             n_step (int): n-step adjustment to discounting.
         """
@@ -80,7 +80,7 @@ class DQNLossFunction(LossFunction):
         self.action_space = action_space
         self.flat_action_space = action_space.flatten()
         # Check for IntBox and num_categories.
-        sanity_check_space(self.action_space, allowed_sub_types=[IntBox], must_have_categories=True)
+        sanity_check_space(self.action_space, must_have_categories=True, allowed_sub_types=[IntBox])
         self.ranks_to_reduce = len(self.action_space.get_shape(with_batch_rank=True)) - 1
 
     @rlgraph_api
@@ -94,22 +94,22 @@ class DQNLossFunction(LossFunction):
     @rlgraph_api
     def loss_per_item(self, q_values_s, actions, rewards, terminals, qt_values_sp, q_values_sp=None,
                       importance_weights=None):
-        # Out may be a ContainerDataOp.
-        loss_per_item = self._graph_fn_loss_per_item(
-            q_values_s, actions, rewards, terminals, qt_values_sp, q_values_sp, importance_weights
-        )
-        # Average over all flat keys in container.
+        # Get the targets.
+        td_targets = self._graph_fn_get_td_targets(rewards, terminals, qt_values_sp, q_values_sp)
+        # Average over container sub-actions.
+        td_targets = self._graph_fn_average_over_container_keys(td_targets)
+
+        # Calculate the loss per item.
+        loss_per_item = self._graph_fn_loss_per_item(td_targets, q_values_s, actions, importance_weights)
+        # Average over container sub-actions.
         loss_per_item = self._graph_fn_average_over_container_keys(loss_per_item)
+
         return loss_per_item
 
     @graph_fn(flatten_ops=True, split_ops=True, add_auto_key_as_first_param=True)
-    def _graph_fn_loss_per_item(self, key, q_values_s, actions, rewards, terminals,
-                                qt_values_sp, q_values_sp=None, importance_weights=None):
+    def _graph_fn_get_td_targets(self, key, rewards, terminals, qt_values_sp, q_values_sp=None):
         """
         Args:
-            q_values_s (SingleDataOp): The batch of Q-values representing the expected accumulated discounted returns
-                when in s and taking different actions a.
-            actions (SingleDataOp): The batch of actions that were actually taken in states s (from a memory).
             rewards (SingleDataOp): The batch of rewards that we received after having taken a in s (from a memory).
             terminals (SingleDataOp): The batch of terminal signals that we received after having taken a in s
                 (from a memory).
@@ -118,12 +118,12 @@ class DQNLossFunction(LossFunction):
             q_values_sp (Optional[SingleDataOp]): If `self.double_q` is True: The batch of Q-values representing the
                 expected accumulated discounted returns (estimated by the (main) policy net) when in s' and taking
                 different actions a'.
-            importance_weights (Optional[SingleDataOp]): If 'self.importance_weights' is True: The batch of weights to
-                apply to the losses.
 
         Returns:
-            SingleDataOp: The loss values vector (one single value for each batch item).
+            SingleDataOp: The target values vector.
         """
+        qt_sp_ap_values = None
+
         # Numpy backend primarily for testing purposes.
         if self.backend == "python" or get_backend() == "python":
             from rlgraph.utils.numpy import one_hot
@@ -138,23 +138,6 @@ class DQNLossFunction(LossFunction):
                 rewards = np.expand_dims(rewards, axis=1)
 
             qt_sp_ap_values = np.where(terminals, np.zeros_like(qt_sp_ap_values), qt_sp_ap_values)
-
-            actions_one_hot = one_hot(actions, depth=self.flat_action_space[key].num_categories)
-            q_s_a_values = np.sum(q_values_s * actions_one_hot, axis=-1)
-
-            td_delta = (rewards + (self.discount ** self.n_step) * qt_sp_ap_values) - q_s_a_values
-
-            if td_delta.ndim > 1:
-                if self.importance_weights:
-                    td_delta = np.mean(
-                        td_delta * importance_weights,
-                        axis=list(range(1, self.ranks_to_reduce + 1))
-                    )
-
-                else:
-                    td_delta = np.mean(td_delta, axis=list(range(1, self.ranks_to_reduce + 1)))
-
-            return self._apply_huber_loss_if_necessary(td_delta)
 
         elif get_backend() == "tf":
             # Make sure the target policy's outputs are treated as constant when calculating gradients.
@@ -183,36 +166,15 @@ class DQNLossFunction(LossFunction):
                 condition=terminals, x=tf.zeros_like(qt_sp_ap_values), y=qt_sp_ap_values
             )
 
-            # Q(s,a) -> Use the Q-value of the action actually taken before.
-            one_hot = tf.one_hot(indices=actions, depth=self.flat_action_space[key].num_categories)
-            q_s_a_values = tf.reduce_sum(input_tensor=(q_values_s * one_hot), axis=-1)
-
-            # Calculate the TD-delta (target - current estimate).
-            td_delta = (rewards + (self.discount ** self.n_step) * qt_sp_ap_values) - q_s_a_values
-
-            # Reduce over the composite actions, if any.
-            if get_rank(td_delta) > 1:
-                td_delta = tf.reduce_mean(input_tensor=td_delta, axis=list(range(1, self.ranks_to_reduce + 1)))
-
-            # Apply importance-weights from a prioritized replay to the loss.
-            if self.importance_weights:
-                return importance_weights * self._apply_huber_loss_if_necessary(td_delta)
-            else:
-                return self._apply_huber_loss_if_necessary(td_delta)
-
         elif get_backend() == "pytorch":
             if not isinstance(terminals, torch.ByteTensor):
                 terminals = terminals.byte()
             # Add batch dim in case of single sample.
-            if q_values_s.dim() == 1:
-                q_values_s = q_values_s.unsqueeze(-1)
-                actions = actions.unsqueeze(-1)
+            if qt_values_sp.dim() == 1:
                 rewards = rewards.unsqueeze(-1)
                 terminals = terminals.unsqueeze(-1)
                 q_values_sp = q_values_sp.unsqueeze(-1)
                 qt_values_sp = qt_values_sp.unsqueeze(-1)
-                if self.importance_weights:
-                    importance_weights = importance_weights.unsqueeze(-1)
 
             # Make sure the target policy's outputs are treated as constant when calculating gradients.
             qt_values_sp = qt_values_sp.detach()
@@ -238,53 +200,112 @@ class DQNLossFunction(LossFunction):
             qt_sp_ap_values = torch.where(
                 terminals, torch.zeros_like(qt_sp_ap_values), qt_sp_ap_values
             )
+
+        td_targets = (rewards + (self.discount ** self.n_step) * qt_sp_ap_values)
+
+        return td_targets
+
+    @graph_fn(flatten_ops=True, split_ops=True, add_auto_key_as_first_param=True)
+    def _graph_fn_loss_per_item(self, key, td_targets, q_values_s, actions, importance_weights=None):
+        """
+        Args:
+            td_targets (SingleDataOp): The already calculated TD-target terms (r + gamma maxa'Qt(s',a')
+                OR for double Q: r + gamma Qt(s',maxa'(Q(s',a'))))
+            q_values_s (SingleDataOp): The batch of Q-values representing the expected accumulated discounted returns
+                when in s and taking different actions a.
+            actions (SingleDataOp): The batch of actions that were actually taken in states s (from a memory).
+            importance_weights (Optional[SingleDataOp]): If 'self.importance_weights' is True: The batch of weights to
+                apply to the losses.
+
+        Returns:
+            SingleDataOp: The loss values vector (one single value for each batch item).
+        """
+        # Numpy backend primarily for testing purposes.
+        if self.backend == "python" or get_backend() == "python":
+            from rlgraph.utils.numpy import one_hot
+
+            actions_one_hot = one_hot(actions, depth=self.flat_action_space[key].num_categories)
+            q_s_a_values = np.sum(q_values_s * actions_one_hot, axis=-1)
+
+            td_delta = td_targets - q_s_a_values
+
+            if td_delta.ndim > 1:
+                if self.importance_weights:
+                    td_delta = np.mean(
+                        td_delta * importance_weights,
+                        axis=list(range(1, self.ranks_to_reduce + 1))
+                    )
+
+                else:
+                    td_delta = np.mean(td_delta, axis=list(range(1, self.ranks_to_reduce + 1)))
+
+        elif get_backend() == "tf":
+            # Q(s,a) -> Use the Q-value of the action actually taken before.
+            one_hot = tf.one_hot(indices=actions, depth=self.flat_action_space[key].num_categories)
+            q_s_a_values = tf.reduce_sum(input_tensor=(q_values_s * one_hot), axis=-1)
+
+            # Calculate the TD-delta (target - current estimate).
+            td_delta = td_targets - q_s_a_values
+
+            # Reduce over the composite actions, if any.
+            if get_rank(td_delta) > 1:
+                td_delta = tf.reduce_mean(input_tensor=td_delta, axis=list(range(1, self.ranks_to_reduce + 1)))
+
+        elif get_backend() == "pytorch":
+            # Add batch dim in case of single sample.
+            if q_values_s.dim() == 1:
+                q_values_s = q_values_s.unsqueeze(-1)
+                actions = actions.unsqueeze(-1)
+                if self.importance_weights:
+                    importance_weights = importance_weights.unsqueeze(-1)
+
             # Q(s,a) -> Use the Q-value of the action actually taken before.
             one_hot = pytorch_one_hot(actions, depth=self.flat_action_space[key].num_categories)
             q_s_a_values = torch.sum((q_values_s * one_hot), -1)
 
             # Calculate the TD-delta (target - current estimate).
-            td_delta = (rewards + (self.discount ** self.n_step) * qt_sp_ap_values) - q_s_a_values
+            td_delta = td_targets - q_s_a_values
 
             # Reduce over the composite actions, if any.
             if get_rank(td_delta) > 1:
                 td_delta = pytorch_reduce_mean(td_delta, list(range(1, self.ranks_to_reduce + 1)), keepdims=False)
 
-            # Apply importance-weights from a prioritized replay to the loss.
-            if self.importance_weights:
-                return importance_weights * self._apply_huber_loss_if_necessary(td_delta)
-            else:
-                return self._apply_huber_loss_if_necessary(td_delta)
+        # Apply importance-weights from a prioritized replay to the loss.
+        if self.importance_weights:
+            return importance_weights * self._apply_huber_loss_if_necessary(td_delta)
+        else:
+            return self._apply_huber_loss_if_necessary(td_delta)
 
     def _apply_huber_loss_if_necessary(self, td_delta):
         if self.backend == "python" or get_backend() == "python":
             if self.huber_loss:
                 return np.where(
-                    condition=np.abs(td_delta) < self.huber_delta,
-                    x=np.square(td_delta) * 0.5,
+                    condition=np.abs(td_delta) <= self.huber_delta,
+                    x=0.5 * np.square(td_delta),
                     y=self.huber_delta * (np.abs(td_delta) - 0.5 * self.huber_delta)
                 )
             else:
-                return np.square(x=td_delta)
+                return 0.5 * np.square(x=td_delta)
         elif get_backend() == "tf":
             if self.huber_loss:
                 return tf.where(
-                    condition=tf.abs(x=td_delta) < self.huber_delta,
-                    x=tf.square(x=td_delta) * 0.5,
+                    condition=tf.abs(x=td_delta) <= self.huber_delta,
+                    x=0.5 * tf.square(x=td_delta),
                     y=self.huber_delta * (tf.abs(x=td_delta) - 0.5 * self.huber_delta)
                 )
             else:
-                return tf.square(x=td_delta)
+                return 0.5 * tf.square(x=td_delta)
         elif get_backend() == "pytorch":
             if self.huber_loss:
                 # Not certain if arithmetics need to be expressed via torch operators.
                 return torch.where(
-                    torch.abs(td_delta) < self.huber_delta,
+                    torch.abs(td_delta) <= self.huber_delta,
                     # PyTorch has no `square`
-                    torch.pow(td_delta, 2) * 0.5,
+                    0.5 * torch.pow(td_delta, 2),
                     self.huber_delta * (torch.abs(td_delta) - 0.5 * self.huber_delta)
                 )
             else:
-                return td_delta * td_delta
+                return 0.5 * td_delta * td_delta
 
     @graph_fn(flatten_ops=True)
     def _graph_fn_average_over_container_keys(self, loss_per_item):
