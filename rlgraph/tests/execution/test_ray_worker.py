46d41a61b1534207498edeb92c30649d55f0dab1
michael <mi.schaarschmidt@gmail.com>
2019-01-20 18:31:53 +0000

    Fix ray sync bugs from api change.

--- rlgraph/tests/execution/test_ray_worker.py
+++ rlgraph/tests/execution/test_ray_worker.py
@@ -176,19 +176,20 @@ class TestRayWorker(unittest.TestCase):
             action_space=env.action_space
         )
 
+        ray_spec["worker_spec"]["worker_sample_size"] = 50
         # Create a remote worker with the same agent config.
         worker = RayWorker.as_remote().remote(agent_config, ray_spec["worker_spec"], self.env_spec,  auto_build=True)
 
         # This imitates the initial executor sync without ray.put
         weights = local_agent.get_weights()
         print('Weight type in init sync = {}'.format(type(weights)))
-        worker.set_weights.remote(weights["policy_weights"], weights["value_function_weights"])
+        worker.set_weights.remote(weights)
         print('Init weight sync successful.')
 
         # Replicate worker syncing steps as done in e.g. Ape-X executor:
         weights = ray.put(local_agent.get_weights())
         print('Weight type returned by ray put = {}'.format(type(weights)))
         print(weights)
-        worker.set_weights.remote(weights["policy_weights"], weights["value_function_weights"])
+        worker.set_weights.remote(weights)
         print('Object store weight sync successful.')
 
