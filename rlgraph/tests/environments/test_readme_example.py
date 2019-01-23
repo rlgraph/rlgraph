38571dfd09b8f463b46edcaadc4623ee8ee70b5f
samialab <6316945+samialab@users.noreply.github.com>
2019-01-23 22:50:15 +0000

    Fix readme example get_action tuple order

--- rlgraph/tests/environments/test_readme_example.py
+++ rlgraph/tests/environments/test_readme_example.py
@@ -26,6 +26,7 @@ class TestReadmeExample(unittest.TestCase):
     """
     Tests if the readme example runs.
     """
+
     def test_readme_example(self):
         """
         Tests deterministic functionality of RandomEnv.
@@ -46,7 +47,7 @@ class TestReadmeExample(unittest.TestCase):
 
         # Get an action, take a step, observe reward.
         state = environment.reset()
-        preprocessed_state, action = agent.get_action(
+        action, preprocessed_state = agent.get_action(
             states=state,
             extra_returns="preprocessed_states"
         )
