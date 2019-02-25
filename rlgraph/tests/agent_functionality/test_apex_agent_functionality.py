e50edacf768af2d111d45b8f9a74c5d0f4e2f667
michael <mi.schaarschmidt@gmail.com>
2019-02-25 17:56:50 +0000

    Add update from external test for apex functionality.

--- rlgraph/tests/agent_functionality/test_apex_agent_functionality.py
+++ rlgraph/tests/agent_functionality/test_apex_agent_functionality.py
@@ -25,6 +25,8 @@ from rlgraph.tests import recursive_assert_almost_equal
 from rlgraph.tests.test_util import config_from_path
 from rlgraph.utils import root_logger
 
+import numpy as np
+
 
 class TestApexAgentFunctionality(unittest.TestCase):
     """
@@ -52,3 +54,25 @@ class TestApexAgentFunctionality(unittest.TestCase):
 
         new_weights = agent.get_weights()["policy_weights"]
         recursive_assert_almost_equal(weights, new_weights)
+
+    def test_update_from_external(self):
+        agent_config = config_from_path("configs/ray_apex_for_pong.json")
+        agent_config["execution_spec"].pop("ray_spec")
+        environment = OpenAIGymEnv("Pong-v0", frameskip=4)
+
+        agent = Agent.from_spec(
+            agent_config,
+            state_space=environment.state_space,
+            action_space=environment.action_space
+        )
+
+        batch = {
+            "states": agent.preprocessed_state_space.sample(200),
+            "actions": environment.action_space.sample(200),
+            "rewards": np.zeros(200, dtype=np.float32),
+            "terminals": [False] * 200,
+            "next_states": agent.preprocessed_state_space.sample(200),
+            "importance_weights":  np.ones(200, dtype=np.float32)
+        }
+
+        agent.update(batch)
