f0d7221207fcce8ecc6880c44995d1e4b96eea89
Sven <svenmika1977@gmail.com>
2019-02-28 09:19:01 +0100

    summary_spec.summary_regexp=None means: no summaries.

--- rlgraph/utils/input_parsing.py
+++ rlgraph/utils/input_parsing.py
@@ -95,7 +95,7 @@ def parse_summary_spec(summary_spec):
         directory=os.path.expanduser("~/rlgraph_summaries/"),  # default=home dir
         # A regexp pattern that a summary op (including its global scope) has to match in order for it to
         # be included in the graph's summaries.
-        summary_regexp="",
+        summary_regexp=None,
         # Every how many seconds do we save a summary? None if saving frequency should be step based.
         save_secs=120,
         # Every how many steps do we save a summary? None if saving frequency should be time (seconds) based.
