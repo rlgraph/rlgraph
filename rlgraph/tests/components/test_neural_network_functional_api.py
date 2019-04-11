36b7f98e1a4b9bc34122f4b8bc976525cbd56e33
sven1977 <svenmika1977@gmail.com>
2019-04-11 14:14:58 +0200

    Add another Keras-style assembly test case (medium complexity).

--- rlgraph/tests/components/test_neural_network_functional_api.py
+++ rlgraph/tests/components/test_neural_network_functional_api.py
@@ -20,12 +20,13 @@ from __future__ import print_function
 import unittest
 
 from rlgraph.components.layers.nn import DenseLayer, LSTMLayer, ConcatLayer, Conv2DLayer
+from rlgraph.components.layers.preprocessing.container_splitter import ContainerSplitter
 from rlgraph.components.layers.preprocessing.reshape import ReShape
 from rlgraph.components.layers.strings import StringToHashBucket, EmbeddingLookup
 from rlgraph.components.neural_networks import NeuralNetwork
 from rlgraph.spaces import Dict, FloatBox, TextBox
 from rlgraph.tests.component_test import ComponentTest
-from rlgraph.utils.numpy import dense_layer, relu
+from rlgraph.utils.numpy import dense_layer, relu, lstm_layer
 
 
 class TestNeuralNetworkFunctionalAPI(unittest.TestCase):
@@ -58,7 +59,34 @@ class TestNeuralNetworkFunctionalAPI(unittest.TestCase):
 
         expected = relu(dense_layer(dense_layer(input_, w1_value, b1_value), w2_value, b2_value))
 
-        test.test(("apply", input_), expected_outputs=expected, decimals=5)
+        test.test(("call", input_), expected_outputs=expected, decimals=5)
+
+        test.terminate()
+
+    def test_functional_api_one_output_is_discarded(self):
+        # Input Space of the network.
+        input_space = FloatBox(shape=(3,), add_batch_rank=True, add_time_rank=True)
+
+        # Pass input through an LSTM and get two outputs (output and internal states), only one of which will be used.
+        lstm_out, _ = LSTMLayer(units=2, return_sequences=False)(input_space)
+
+        # A NN with 1 output (don't return internal_states of LSTM).
+        neural_net = NeuralNetwork(outputs=lstm_out)
+
+        test = ComponentTest(component=neural_net, input_spaces=dict(inputs=input_space))
+
+        # Batch of size=n.
+        input_ = input_space.sample((5, 3))
+        # Calculate output manually.
+        var_dict = neural_net.variable_registry
+        w1_value = test.read_variable_values(var_dict["neural-network/lstm-layer/lstm-cell/kernel"])
+        b1_value = test.read_variable_values(var_dict["neural-network/lstm-layer/lstm-cell/bias"])
+
+        expected_out, _ = lstm_layer(input_, w1_value, b1_value)
+        expected_out = expected_out[:, -1, :]  # last time step only
+
+        # Don't expect internal states (our NN does not return these as per the functional API definition above).
+        test.test(("call", input_), expected_outputs=expected_out, decimals=5)
 
         test.terminate()
 
@@ -69,48 +97,52 @@ class TestNeuralNetworkFunctionalAPI(unittest.TestCase):
             "txt": TextBox()  # some text
         }, add_batch_rank=True, add_time_rank=True)
 
+        img, txt = ContainerSplitter("img", "txt")()
         # Complex NN assembly via our Keras-style functional API.
         # Fold text input into single batch rank.
-        folded_text = ReShape(fold_time_rank=True)(input_space["txt"])
+        folded_text = ReShape(fold_time_rank=True)(txt)
         # String layer will create batched AND time-ranked (individual words) hash outputs (int64).
         string_bucket_out, lengths = StringToHashBucket(num_hash_buckets=5)(folded_text)
         # Batched and time-ranked embedding output (floats) with embed dim=n.
         embedding_out = EmbeddingLookup(embed_dim=10, vocab_size=5)(string_bucket_out)
         # Pass embeddings through a text LSTM and use last output (reduce time-rank).
-        string_lstm_out = LSTMLayer(units=2, return_sequences=False)(embedding_out, sequence_length=lengths)
+        string_lstm_out, _ = LSTMLayer(units=2, return_sequences=False, scope="lstm-layer-txt")(
+            embedding_out, sequence_length=lengths
+        )
         # Unfold to get original time-rank back.
         # TODO: Let NN handle orig. input passing for unfolding into this layer.
         string_lstm_out_unfolded = ReShape(unfold_time_rank=True)(string_lstm_out)
 
         # Parallel image stream via 1 CNN layer plus dense.
-        cnn_out = Conv2DLayer(filters=1, kernel_size=2, strides=2)(input_space["img"])
-        dense_out = DenseLayer(units=2)(cnn_out)
+        cnn_out = Conv2DLayer(filters=1, kernel_size=2, strides=2)(img)
+        dense_out = DenseLayer(units=2, scope="dense-0")(cnn_out)
 
         # Concat everything.
         concat_out = ConcatLayer()(string_lstm_out_unfolded, dense_out)
 
         # LSTM output has batch+time.
-        main_lstm_out = LSTMLayer(units=2)(concat_out)
+        main_lstm_out, internal_states = LSTMLayer(units=2, scope="lstm-layer-main")(concat_out)
 
-        dense_after_lstm_out = DenseLayer(units=3)(main_lstm_out)
+        dense1_after_lstm_out = DenseLayer(units=3, scope="dense-1")(main_lstm_out)
+        dense2_after_lstm_out = DenseLayer(units=2, scope="dense-2")(dense1_after_lstm_out)
+        dense3_after_lstm_out = DenseLayer(units=1, scope="dense-3")(dense2_after_lstm_out)
 
         # A NN with 2 outputs.
-        neural_net = NeuralNetwork(outputs=[dense_after_lstm_out, main_lstm_out])
+        neural_net = NeuralNetwork(outputs=[dense3_after_lstm_out, main_lstm_out, internal_states], inputs=input_space)
 
-        # Do not seed, we calculate expectations manually.
-        test = ComponentTest(component=neural_net, input_spaces=dict(nn_input=input_space))
+        test = ComponentTest(component=neural_net, input_spaces=dict(inputs=input_space))
 
         # Batch of size=n.
-        input_ = input_space.sample(5)
+        input_ = input_space.sample((4, 2))
         # Calculate output manually.
-        var_dict = neural_net.get_variables()
-        w1_value = test.read_params("neural-network/a/dense/kernel", var_dict)
-        b1_value = test.read_params("neural-network/a/dense/bias", var_dict)
-        w2_value = test.read_params("neural-network/b/dense/kernel", var_dict)
-        b2_value = test.read_params("neural-network/b/dense/bias", var_dict)
+        var_dict = neural_net.variable_registry
+        w1_value = test.read_variable_values(var_dict["neural-network/a/dense/kernel"])
+        b1_value = test.read_variable_values(var_dict["neural-network/a/dense/bias"])
+        w2_value = test.read_variable_values(var_dict["neural-network/b/dense/kernel"])
+        b2_value = test.read_variable_values(var_dict["neural-network/b/dense/bias"])
 
         expected = relu(dense_layer(dense_layer(input_, w1_value, b1_value), w2_value, b2_value))
 
-        test.test(("apply", input_), expected_outputs=dict(output=expected), decimals=5)
+        test.test(("call", input_), expected_outputs=dict(output=expected), decimals=5)
 
         test.terminate()
