Fix all remaining bugs related to JointCumulativeDistribution component and log-likelihood tests.

rlgraph/components/distributions/joint_cumulative_distribution.py
@@ -19,6 +19,7 @@ from __future__ import print_function
 
 from rlgraph import get_backend
 from rlgraph.components.distributions.distribution import Distribution
+from rlgraph.components.layers.preprocessing.reshape import ReShape
 from rlgraph.utils.decorators import rlgraph_api, graph_fn
 from rlgraph.utils.ops import flatten_op
 
@@ -30,51 +31,98 @@ elif get_backend() == "pytorch":
 
 class JointCumulativeDistribution(Distribution):
     """
-    A joint cumulative distribution consisting of an arbitrarily nested container of n independent sub-distributions
-    assumed to be all independent of each other, such that:
-    For e.g. n=2 and random variables X and Y: P(X and Y) = P(X)*P(Y)) for all x and y.
+    A joint cumulative distribution consisting of an arbitrarily nested container of n sub-distributions
+    assumed to be all independent(!) of each other, such that:
+    For e.g. n=2 and random variables X and Y: P(X and Y) = P(X)*P(Y) for all x and y.
     - Sampling returns a ContainerDataOp.
     - log_prob returns the sum of all single log prob terms (joint log prob).
+    - entropy returns the sum of all single entropy terms (joint entropy).
     """
-    def __init__(self, sub_distributions_spec, scope="joint-cumulative-distribution", **kwargs):
+    def __init__(self, distribution_specs, scope="joint-cumulative-distribution", **kwargs):
         """
         Args:
-            sub_distributions_spec (Union[tuple,dict]): Tuple of dict (possibly nested) with the specifications of the
-                single sub-distributions.
+            distribution_specs (dict): Dict with flat-keys containing the specifications of the single
+                sub-distributions.
         """
         super(JointCumulativeDistribution, self).__init__(scope=scope, **kwargs)
 
         # Create the flattened sub-distributions and add them.
-        flattened_sub_distributions = flatten_op(sub_distributions_spec)
         self.flattened_sub_distributions = \
-            {k: Distribution.from_spec(s, scope="sub-distribution-{}".format(i))
-             for i, (k, s) in enumerate(flattened_sub_distributions.items())
+            {flat_key: Distribution.from_spec(spec, scope="sub-distribution-{}".format(i))
+             for i, (flat_key, spec) in enumerate(distribution_specs.items())
             }
+        self.flattener = ReShape(flatten=True)
 
-        self.add_components(*list(self.flattened_sub_distributions.values()))
+        self.add_components(self.flattener, *list(self.flattened_sub_distributions.values()))
+
+    @rlgraph_api
+    def log_prob(self, parameters, values):
+        """
+        Override log_prob API as we have to add all the resulting log-probs together
+        (joint log-prob of individual ones).
+        """
+        distributions = self.get_distribution(parameters)
+        all_log_probs = self._graph_fn_log_prob(distributions, values)
+        return self._graph_fn_reduce_over_sub_distributions(all_log_probs)
 
     # Flatten only alongside `self.flattened_sub_distributions`, not any further.
-    @rlgraph_api(flatten_ops="flattened_sub_distributions", split_ops=True, add_auto_key_as_first_param=True)
+    @rlgraph_api(flatten_ops="flattened_sub_distributions", split_ops=True, add_auto_key_as_first_param=True, ok_to_overwrite=True)
     def _graph_fn_get_distribution(self, key, parameters):
-        """
-        Args:
-            parameters (DataOpTuple): Tuple holding the mean and stddev parameters.
-        """
         return self.flattened_sub_distributions[key].get_distribution(parameters)
 
-    # Flatten only alongside `self.flattened_sub_distributions`, not any further.
-    @graph_fn(flatten_ops="flattened_sub_distributions", split_ops=True, add_auto_key_as_first_param=True)
+    @graph_fn(flatten_ops=True, split_ops=True, add_auto_key_as_first_param=True)
     def _graph_fn_sample_deterministic(self, key, distribution):
-        return self.flattened_sub_distributions[key].sample_deterministic(distribution)
+        return self.flattened_sub_distributions[key]._graph_fn_sample_deterministic(distribution)
+
+    @graph_fn(flatten_ops=True, split_ops=True, add_auto_key_as_first_param=True)
+    def _graph_fn_sample_stochastic(self, key, distribution):
+        return self.flattened_sub_distributions[key]._graph_fn_sample_stochastic(distribution)
+
+    # Flatten only alongside `self.flattened_sub_distributions`, not any further.
+    @graph_fn(flatten_ops=True, split_ops=True, add_auto_key_as_first_param=True)
+    def _graph_fn_log_prob(self, key, distribution, values):
+        return self.flattened_sub_distributions[key]._graph_fn_log_prob(distribution, values)
+
+    @graph_fn(flatten_ops=True)
+    def _graph_fn_reduce_over_sub_distributions(self, log_probs):
+        params_space = next(iter(flatten_op(self.api_method_inputs["parameters"]).values()))
+        num_ranks_to_keep = (1 if params_space.has_batch_rank else 0) + (1 if params_space.has_time_rank else 0)
+        log_probs_list = []
+        if get_backend() == "tf":
+            for log_prob in log_probs.values():
+                # Reduce sum over all ranks to get the joint log llh.
+                log_prob = tf.reduce_sum(log_prob, axis=list(range(len(log_prob.shape) - 1, num_ranks_to_keep - 1, -1)))
+                log_probs_list.append(log_prob)
+            return tf.reduce_sum(tf.stack(log_probs_list, axis=0), axis=0)
+
+        elif get_backend() == "pytorch":
+            for log_prob in log_probs.values():
+                # Reduce sum over all ranks to get the joint log llh.
+                log_prob = torch.sum(log_prob, dim=list(range(len(log_prob.shape) - 1, num_ranks_to_keep - 1, -1)))
+                log_probs_list.append(log_prob)
+
+            return torch.sum(torch.stack(log_probs_list, dim=0), dim=0)
 
     # Flatten only alongside `self.flattened_sub_distributions`, not any further.
     @graph_fn(flatten_ops="flattened_sub_distributions")
-    def _graph_fn_log_prob(self, distribution, values):
-        all_log_probs = []
-        for key, distr in distribution.items():
-            all_log_probs.append(distr.log_prob(values[key]))
-
+    def _graph_fn_entropy(self, distribution):
+        params_space = next(iter(flatten_op(self.api_method_inputs["parameters"]).values()))
+        num_ranks_to_keep = (1 if params_space.has_batch_rank else 0) + (1 if params_space.has_time_rank else 0)
+        all_entropies = []
         if get_backend() == "tf":
-            return tf.reduce_sum(tf.stack(all_log_probs, axis=0), axis=0)
+            for key, distr in distribution.items():
+                entropy = distr.entropy()
+                # Reduce sum over all ranks to get the joint log llh.
+                entropy = tf.reduce_sum(entropy, axis=list(range(len(entropy.shape) - 1, num_ranks_to_keep - 1, -1)))
+                all_entropies.append(entropy)
+            return tf.reduce_sum(tf.stack(all_entropies, axis=0), axis=0)
+
         elif get_backend() == "pytorch":
-            return torch.sum(torch.stack(all_log_probs, dim=0), dim=0)
+            for key, distr in distribution.items():
+                entropy = distr.entropy()
+                # Reduce sum over all ranks to get the joint log llh.
+                entropy = torch.sum(entropy, dim=list(range(len(entropy.shape) - 1, num_ranks_to_keep - 1, -1)))
+                all_entropies.append(entropy)
+
+            # TODO: flatten all all_log_probs (or expand in last dim) so we can concat, then reduce_sum to get the joint probs.
+            return torch.sum(torch.stack(all_entropies, dim=0), dim=0)

rlgraph/tests/components/test_distributions.py
@@ -20,12 +20,11 @@ from __future__ import print_function
 import unittest
 
 import numpy as np
-from scipy.stats import norm, beta
-
 from rlgraph.components.distributions import *
 from rlgraph.spaces import *
 from rlgraph.tests import ComponentTest, recursive_assert_almost_equal
 from rlgraph.utils.numpy import softmax
+from scipy.stats import norm, beta
 
 
 class TestDistributions(unittest.TestCase):
@@ -222,21 +221,22 @@ class TestDistributions(unittest.TestCase):
         )
 
         # The Component to test.
-        beta_distribution = Beta(switched_off_apis={"kl_divergence"})
+        low, high = -1.0, 2.0
+        beta_distribution = Beta(low=low, high=high, switched_off_apis={"kl_divergence"})
         test = ComponentTest(component=beta_distribution, input_spaces=input_spaces)
 
         # Batch of size=2 and deterministic (True).
         input_ = [input_spaces["parameters"].sample(2), True]
         # Mean for a Beta distribution: 1 / [1 + (beta/alpha)]
-        expected = 1.0 / (1.0 + input_[0][1] / input_[0][0])
+        expected = (1.0 / (1.0 + input_[0][1] / input_[0][0])) * (high - low) + low
         # Sample n times, expect always mean value (deterministic draw).
         for _ in range(50):
-            test.test(("draw", input_), expected_outputs=expected)
-            test.test(("sample_deterministic", tuple([input_[0]])), expected_outputs=expected)
+            test.test(("draw", input_), expected_outputs=expected, decimals=5)
+            test.test(("sample_deterministic", tuple([input_[0]])), expected_outputs=expected, decimals=5)
 
         # Batch of size=1 and non-deterministic -> expect roughly the mean.
         input_ = [input_spaces["parameters"].sample(1), False]
-        expected = 1.0 / (1.0 + input_[0][1] / input_[0][0])
+        expected = (1.0 / (1.0 + input_[0][1] / input_[0][0])) * (high - low) + low
         outs = []
         for _ in range(50):
             out = test.test(("draw", input_))
@@ -247,12 +247,13 @@ class TestDistributions(unittest.TestCase):
         recursive_assert_almost_equal(np.mean(outs), expected.mean(), decimals=1)
 
         # Test log-likelihood outputs (against scipy).
-        means = values_space.sample(1)
-        stds = values_space.sample(1)
+        alpha_ = values_space.sample(1)
+        beta_ = values_space.sample(1)
         values = values_space.sample(1)
+        values_scaled = values * (high - low) + low
         test.test(
-            ("log_prob", [tuple([means, stds]), values]),
-            expected_outputs=np.log(beta.pdf(values, means, stds)), decimals=4
+            ("log_prob", [tuple([alpha_, beta_]), values_scaled]),
+            expected_outputs=np.log(beta.pdf(values, alpha_, beta_)), decimals=4
         )
 
     def test_mixture(self):
@@ -398,24 +399,89 @@ class TestDistributions(unittest.TestCase):
     def test_joint_cumulative_distribution(self):
         param_space = Dict({
             "a": FloatBox(shape=(4,)),  # 4-discrete
-            "b": Tuple([FloatBox(shape=(9,)), FloatBox(shape=(9,))])  # 9-variate normal
+            "b": Dict({"ba": Tuple([FloatBox(shape=(3,)), FloatBox(0.1, 1.0, shape=(3,))]),  # 3-variate normal
+                       "bb": Tuple([FloatBox(shape=(2,)), FloatBox(shape=(2,))]),  # beta -1 to 1
+                       "bc": Tuple([FloatBox(shape=(4,)), FloatBox(0.1, 1.0, shape=(4,))]),  # normal (dim=4)
+                       })
         }, add_batch_rank=True)
 
-        values_space = Dict({"a": IntBox(4), "b": FloatBox(shape=(9,))}, add_batch_rank=True)
+        values_space = Dict({
+            "a": IntBox(4),
+            "b": Dict({
+                "ba": FloatBox(shape=(3,)),
+                "bb": FloatBox(shape=(2,)),
+                "bc": FloatBox(shape=(4,))
+            })
+        }, add_batch_rank=True)
 
         input_spaces = dict(
             parameters=param_space,
-            deterministic=bool,
-            values=values_space
+            values=values_space,
+            deterministic=bool
         )
 
-        joined_cumulative_distribution = JointCumulativeDistribution(sub_distributions_spec=dict(
-            a=Categorical(), b=Normal()
-        ), switched_off_apis={"kl_divergence"})
+        low, high = -1.0, 1.0
+        joined_cumulative_distribution = JointCumulativeDistribution(distribution_specs={
+            "/a": Categorical(), "/b/ba": MultivariateNormal(), "/b/bb": Beta(low=low, high=high), "/b/bc": Normal()
+        }, switched_off_apis={"kl_divergence"})
         test = ComponentTest(component=joined_cumulative_distribution, input_spaces=input_spaces)
 
-        input_ = [param_space.sample(5), values_space.sample(5)]
+        # Batch of size=2 and deterministic (True).
+        input_ = [param_space.sample(2), True]
+        input_[0]["a"] = softmax(input_[0]["a"])
+        expected_mean = {
+            "a": np.argmax(input_[0]["a"], axis=-1),
+            "b": {
+                "ba": input_[0]["b"]["ba"][0],  # [0]=Mean
+                # Mean for a Beta distribution: 1 / [1 + (beta/alpha)] * range + low
+                "bb": (1.0 / (1.0 + input_[0]["b"]["bb"][1] / input_[0]["b"]["bb"][0])) * (high - low) + low,
+                "bc": input_[0]["b"]["bc"][0],
+            }
+        }
+        # Sample n times, expect always mean value (deterministic draw).
+        for _ in range(50):
+            test.test(("draw", input_), expected_outputs=expected_mean)
+            test.test(("sample_deterministic", tuple([input_[0]])), expected_outputs=expected_mean)
 
-        out = test.test(("log_prob", input_), expected_outputs=None)
+        # Batch of size=1 and non-deterministic -> expect roughly the mean.
+        input_ = [param_space.sample(1), False]
+        input_[0]["a"] = softmax(input_[0]["a"])
+        expected_mean = {
+            "a": np.sum(input_[0]["a"] * np.array([0, 1, 2, 3])),
+            "b": {
+                "ba": input_[0]["b"]["ba"][0],  # [0]=Mean
+                # Mean for a Beta distribution: 1 / [1 + (beta/alpha)] * range + low
+                "bb": (1.0 / (1.0 + input_[0]["b"]["bb"][1] / input_[0]["b"]["bb"][0])) * (high - low) + low,
+                "bc": input_[0]["b"]["bc"][0],
+            }
+        }
 
-        print(out)
+        outs = []
+        for _ in range(100):
+            out = test.test(("draw", input_))
+            outs.append(out)
+            out = test.test(("sample_stochastic", tuple([input_[0]])))
+            outs.append(out)
+
+        recursive_assert_almost_equal(np.mean(np.stack([o["a"][0] for o in outs], axis=0), axis=0), expected_mean["a"], atol=0.2)
+        recursive_assert_almost_equal(np.mean(np.stack([o["b"]["ba"][0] for o in outs], axis=0), axis=0),
+                                      expected_mean["b"]["ba"][0], decimals=1)
+        recursive_assert_almost_equal(np.mean(np.stack([o["b"]["bb"][0] for o in outs], axis=0), axis=0),
+                                      expected_mean["b"]["bb"][0], decimals=1)
+        recursive_assert_almost_equal(np.mean(np.stack([o["b"]["bc"][0] for o in outs], axis=0), axis=0),
+                                      expected_mean["b"]["bc"][0], decimals=1)
+
+        # Test log-likelihood outputs.
+        params = param_space.sample(1)
+        params["a"] = softmax(params["a"])
+        # Make sure beta-values are within 0.0 and 1.0 for the numpy calculation (which doesn't have scaling).
+        values = values_space.sample(1)
+        log_prob_beta = np.log(beta.pdf(values["b"]["bb"], params["b"]["bb"][0], params["b"]["bb"][1]))
+        # Now do the scaling for b/bb (beta values).
+        values["b"]["bb"] = values["b"]["bb"] * (high - low) + low
+        expected_log_llh = np.log(params["a"][0][values["a"][0]]) + \
+            np.sum(np.log(norm.pdf(values["b"]["ba"][0], params["b"]["ba"][0], params["b"]["ba"][1]))) + \
+            np.sum(log_prob_beta) + \
+            np.sum(np.log(norm.pdf(values["b"]["bc"][0], params["b"]["bc"][0], params["b"]["bc"][1])))
+
+        test.test(("log_prob", [params, values]), expected_outputs=expected_log_llh, decimals=1)