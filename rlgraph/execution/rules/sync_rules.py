3e5fa5317010ff783e629dd4cf7e4a2d5f3d09ab
sven1977 <svenmika1977@gmail.com>
2019-05-23 09:16:34 +0200

    Add bounds check to SyncRules.sync_tau (0.0 and 1.0).

--- rlgraph/execution/rules/sync_rules.py
+++ rlgraph/execution/rules/sync_rules.py
@@ -25,13 +25,16 @@ class SyncRules(Specifiable):
     Describes a synchronization schedule, used to update the target value weights. The target values are gradually
     updates using exponential moving average as suggested by the paper.
     """
-    def __init__(self, sync_every_n_updates=4, sync_tau=None):
+    def __init__(self, sync_every_n_updates=4, sync_tau=1.0):
         """
         Args:
             sync_every_n_updates (int): Every how many updates (calls to `Agent.update()`) do we update the target?
-            sync_tau (int): The smoothing constant to use in the averaging. Setting to 1 replaces the values each iteration.
+            sync_tau (float): The smoothing constant (between 0.0 and 1.0) to use in the averaging.
+                Setting to 1.0 replaces the values each iteration. Default: 1.0.
         """
         super(SyncRules, self).__init__()
 
         self.sync_every_n_updates = sync_every_n_updates
+
+        assert 0.0 < sync_tau <= 1.0, "ERROR: `sync_tau` ({}) must be in interval (0.0, 1.0]!".format(sync_tau)
         self.sync_tau = sync_tau
