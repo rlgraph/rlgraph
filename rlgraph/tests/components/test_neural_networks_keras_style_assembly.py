2726a69d5b83eea541ef2c49fb02ac0d090c3700
Sven <svenmika1977@gmail.com>
2019-06-02 23:00:20 +0200

    Added one more test case for Keras-style NN assembly.

--- rlgraph/tests/components/test_neural_networks_keras_style_assembly.py
+++ rlgraph/tests/components/test_neural_networks_keras_style_assembly.py
@@ -27,11 +27,11 @@ from rlgraph.tests.component_test import ComponentTest
 from rlgraph.utils.numpy import dense_layer, relu, lstm_layer, one_hot
 
 
-class TestNeuralNetworkFunctionalAPI(unittest.TestCase):
+class TestNeuralNetworkKerasStyleAssembly(unittest.TestCase):
     """
     Tests for assembling from json and running different NeuralNetworks.
     """
-    def test_functional_api_simple_nn(self):
+    def test_keras_style_simple_nn(self):
         # Input Space of the network.
         input_space = FloatBox(shape=(3,), add_batch_rank=True)
 
@@ -61,7 +61,7 @@ class TestNeuralNetworkFunctionalAPI(unittest.TestCase):
 
         test.terminate()
 
-    def test_functional_api_one_output_is_discarded(self):
+    def test_keras_style_one_output_is_discarded(self):
         # Input Space of the network.
         input_space = FloatBox(shape=(3,), add_batch_rank=True, add_time_rank=True)
 
@@ -88,7 +88,7 @@ class TestNeuralNetworkFunctionalAPI(unittest.TestCase):
 
         test.terminate()
 
-    def test_functional_api_two_separate_input_spaces(self):
+    def test_keras_style_two_separate_input_spaces(self):
         # Define two input Spaces first. Independently (no container).
         input_space_1 = IntBox(3, add_batch_rank=True)
         input_space_2 = FloatBox(shape=(4,), add_batch_rank=True)
@@ -123,7 +123,7 @@ class TestNeuralNetworkFunctionalAPI(unittest.TestCase):
 
         test.terminate()
 
-    def test_functional_api_one_container_input_space(self):
+    def test_keras_style_one_container_input_space(self):
         # Define one container input Space.
         input_space = Tuple(IntBox(3), FloatBox(shape=(4,)), add_batch_rank=True)
 
@@ -158,7 +158,7 @@ class TestNeuralNetworkFunctionalAPI(unittest.TestCase):
 
         test.terminate()
 
-    def test_functional_api_multi_stream_nn(self):
+    def test_keras_style_multi_stream_nn(self):
         # Input Space of the network.
         input_space = Dict({
             "img": FloatBox(shape=(6, 6, 3)),  # some RGB img
@@ -206,6 +206,73 @@ class TestNeuralNetworkFunctionalAPI(unittest.TestCase):
         input_ = input_space.sample(sample_shape)
 
         out = test.test(("call", input_), expected_outputs=None)
+        # Main output (Dense out after LSTM).
+        self.assertTrue(out[0].shape == sample_shape + (1,))  # 1=1 unit in dense layer
+        self.assertTrue(out[0].dtype == np.float32)
+        # main-LSTM out.
+        self.assertTrue(out[1].shape == sample_shape + (2,))  # 2=2 LSTM units
+        self.assertTrue(out[1].dtype == np.float32)
+        # main-LSTM internal-states.
+        self.assertTrue(out[2][0].shape == sample_shape[:1] + (2,))  # 2=2 LSTM units
+        self.assertTrue(out[2][0].dtype == np.float32)
+        self.assertTrue(out[2][1].shape == sample_shape[:1] + (2,))  # 2=2 LSTM units
+        self.assertTrue(out[2][1].dtype == np.float32)
+
+        test.terminate()
+
+    def test_keras_style_complex_multi_stream_nn(self):
+        # 3 inputs.
+        input_spaces = [
+            Dict({
+                "img": FloatBox(shape=(6, 6, 3)),
+                "int": IntBox(3)
+            }, add_batch_rank=True, add_time_rank=True),
+            FloatBox(shape=(2,), add_batch_rank=True),
+            Tuple(IntBox(2), TextBox(), add_batch_rank=True, add_time_rank=True)
+        ]
+
+        # Same NN as in test above, only using some of the sub-Spaces from the input spaces.
+        # Tests whether this NN can add automatically the correct splitters.
+        folded_text = ReShape(fold_time_rank=True)(input_spaces[2][1])
+        # String layer will create batched AND time-ranked (individual words) hash outputs (int64).
+        string_bucket_out, lengths = StringToHashBucket(num_hash_buckets=5)(folded_text)
+        # Batched and time-ranked embedding output (floats) with embed dim=n.
+        embedding_out = EmbeddingLookup(embed_dim=10, vocab_size=5)(string_bucket_out)
+        # Pass embeddings through a text LSTM and use last output (reduce time-rank).
+        string_lstm_out, _ = LSTMLayer(units=2, return_sequences=False, scope="lstm-layer-txt")(
+            embedding_out, sequence_length=lengths
+        )
+        # Unfold to get original time-rank back.
+        string_lstm_out_unfolded = ReShape(unfold_time_rank=True)(string_lstm_out, input_spaces[2][1])
+
+        # Parallel image stream via 1 CNN layer plus dense.
+        folded_img = ReShape(fold_time_rank=True, scope="img-fold")(input_spaces[0]["img"])
+        cnn_out = Conv2DLayer(filters=1, kernel_size=2, strides=2)(folded_img)
+        unfolded_cnn_out = ReShape(unfold_time_rank=True, scope="img-unfold")(cnn_out, input_spaces[0]["img"])
+        unfolded_cnn_out_flattened = ReShape(flatten=True, scope="img-flat")(unfolded_cnn_out)
+        dense_out = DenseLayer(units=2, scope="dense-0")(unfolded_cnn_out_flattened)
+
+        # Concat everything.
+        concat_out = ConcatLayer()(string_lstm_out_unfolded, dense_out)
+
+        # LSTM output has batch+time.
+        main_lstm_out, internal_states = LSTMLayer(units=2, scope="lstm-layer-main")(concat_out)
+
+        dense1_after_lstm_out = DenseLayer(units=3, scope="dense-1")(main_lstm_out)
+        dense2_after_lstm_out = DenseLayer(units=2, scope="dense-2")(dense1_after_lstm_out)
+        dense3_after_lstm_out = DenseLayer(units=1, scope="dense-3")(dense2_after_lstm_out)
+
+        # A NN with 3 outputs.
+        neural_net = NeuralNetwork(inputs=input_spaces, outputs=[dense3_after_lstm_out, main_lstm_out, internal_states])
+
+        test = ComponentTest(component=neural_net, input_spaces=dict(inputs=input_spaces))
+
+        # Batch of size=n.
+        sample_shape = (4, 2)
+        input_ = [input_spaces[0].sample(sample_shape), input_spaces[1].sample(sample_shape[0]),
+                  input_spaces[2].sample(sample_shape)]
+
+        out = test.test(("call", tuple(input_)), expected_outputs=None)
         # Main output (Dense out after LSTM).
         self.assertTrue(out[0].shape == sample_shape + (1,))  # 1=1 unit in dense layer
         self.assertTrue(out[0].dtype == np.float32)
