numpy utils: - softmax now works exactly like in tf code. - one_hot: bug fix for boolean inputs (one-hot to [0, 1]=True and [1, 0]=False).

rlgraph/utils/numpy.py
@@ -18,6 +18,7 @@ from __future__ import division
 from __future__ import print_function
 
 import numpy as np
+from rlgraph.utils.util import SMALL_NUMBER
 
 
 def sigmoid(x, derivative=False):
@@ -43,9 +44,6 @@ def softmax(x, axis=-1):
     Returns the softmax values for x as:
     S(xi) = e^xi / SUMj(e^xj), where j goes over all elements in x.
 
-    Thanks to alvas for the trick with the max for numerical stability:
-    https://stackoverflow.com/questions/34968722/how-to-implement-the-softmax-function-in-python
-
     Args:
         x (np.ndarray): The input to the softmax function.
         axis (int): The axis along which to softmax.
@@ -53,8 +51,9 @@ def softmax(x, axis=-1):
     Returns:
         np.ndarray: The softmax over x.
     """
-    e_x = np.exp(x - np.max(x, axis=axis, keepdims=True))
-    return e_x / np.sum(e_x, axis=axis, keepdims=True)
+    # Below is more like what we do in RLgraph's tf implementations.
+    x_exp = np.exp(x)
+    return np.maximum(x_exp / np.sum(x_exp, axis, keepdims=True), SMALL_NUMBER)
 
 
 def relu(x, alpha=0.0):
@@ -79,7 +78,7 @@ def one_hot(x, depth=0, on_value=1, off_value=0):
     https://gist.github.com/qianyizhang/07ee1c15cad08afb03f5de69349efc30.
 
     Args:
-        x (np.ndarray): The input to be one-hot.
+        x (np.ndarray): The input to be one-hot encoded.
         depth (int): The max. number to be one-hot encoded (size of last rank).
         on_value (float): The value to use for on. Default: 1.0.
         off_value (float): The value to use for off. Default: 0.0.
@@ -87,6 +86,11 @@ def one_hot(x, depth=0, on_value=1, off_value=0):
     Returns:
         np.ndarray: The one-hot encoded equivalent of the input array.
     """
+    # Handle bool arrays correctly.
+    if x.dtype == np.bool_:
+        x = x.astype(np.int)
+        depth = 2
+
     if depth == 0:
         depth = np.max(x) + 1
     assert np.max(x) < depth, "ERROR: The max. index of `x` ({}) is larger than depth ({})!".format(np.max(x), depth)