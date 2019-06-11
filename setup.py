460e25fdb9f19d6e417f62830a514de1a2bbd9a6
Sven <svenmika1977@gmail.com>
2019-06-11 13:33:54 +0200

    Added packaging requirement to setup.py. Needed to not break all LSTMLayers due to the not-supported vs required dtype arg for tf versions >1.12 vs <=1.12.

--- setup.py
+++ setup.py
@@ -13,9 +13,7 @@
 # limitations under the License.
 # ==============================================================================
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
+from __future__ import absolute_import, division, print_function
 
 import os
 
@@ -30,6 +28,7 @@ install_requires = [
     'absl-py',
     'numpy',
     'opencv-python',
+    'packaging',  # Needed for version string comparisons.
     'pyyaml',
     'pytest',
     'requests',
