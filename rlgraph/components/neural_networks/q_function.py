dc6d68873a48641dabbca0367e3cd05c3fc53c93
sven1977 <svenmika1977@gmail.com>
2019-07-03 14:18:06 +0200

    Start developing QFunction Component (should replace SACNetworks soon). WIP.

--- rlgraph/components/neural_networks/q_function.py
+++ rlgraph/components/neural_networks/q_function.py
@@ -15,7 +15,7 @@
 
 from __future__ import absolute_import, division, print_function
 
-from rlgraph.components.layers import Layer, ConcatLayer
+from rlgraph.components.layers import ConcatLayer
 from rlgraph.components.neural_networks.neural_network import NeuralNetwork
 from rlgraph.components.neural_networks.value_function import ValueFunction
 from rlgraph.utils.decorators import rlgraph_api
@@ -23,64 +23,37 @@ from rlgraph.utils.decorators import rlgraph_api
 
 class QFunction(ValueFunction):
     """
-    A Q-function network taking state and action inputs.
+    A Q-function network taking state and action inputs and concatenating them either at the very beginning OR -
+    if an extra image_stack_spec is given - only after passing the states through this image_stack.
     """
-    def __init__(self, scope="q-function", **kwargs):
+    def __init__(self, *layers, **kwargs):
         self.image_stack = None
-        self.dense_stack = None
-
-        super(QFunction, self).__init__(scope=scope, **kwargs)
-
-        # Add all sub-components to this one.
-        #if self.image_stack is not None:
-        #    self.add_components(self.image_stack)
-        self.concat_layer = ConcatLayer()
-        #self.add_components(self.concat_layer, self.dense_stack)
-
-    def build_value_function(self):
-        """
-        Builds a dense stack and optionally an image stack.
-        """
-        if self.image_stack is not None:
-            image_components = []
-            dense_components = []
-            for layer_spec in self.network_spec:
-                if layer_spec["type"] in ["conv2d", "reshape"]:
-                    image_components.append(Layer.from_spec(layer_spec))
-
-            self.image_stack = NeuralNetwork(*image_components, scope="image-stack")
-
-            # Remaining layers should be dense.
-            for layer_spec in self.network_spec[len(image_components):]:
-                assert layer_spec["type"] == "dense", "Only expecting dense layers after image " \
-                                                      "stack but found spec: {}.".format(layer_spec)
-                dense_components.append(layer_spec)
-
-            dense_components.append(self.value_layer_spec)
-            self.dense_stack = NeuralNetwork(*dense_components, scope="dense-stack")
-            self.neural_network = NeuralNetwork(self.image_stack, self.dense_stack)
+        image_stack_spec = kwargs.pop("image_stack_spec", None)
+        if image_stack_spec is not None:
+            self.image_stack = NeuralNetwork.from_spec(image_stack_spec)
+        main_nn_spec = kwargs.pop("network_spec", None)
+        if main_nn_spec is not None:
+            self.main_nn = NeuralNetwork.from_spec(main_nn_spec)
         else:
-            # Assume dense network otherwise -> only a single stack.
-            dense_components = []
-            for layer_spec in self.network_spec:
-                assert layer_spec["type"] == "dense", \
-                    "ERROR: Only dense layers allowed if not using image stack in this network."
-                dense_components.append(Layer.from_spec(layer_spec))
-            dense_components.append(self.value_layer_spec)
-            self.dense_stack = NeuralNetwork(*dense_components, scope="dense-stack")
+            self.main_nn = NeuralNetwork(*layers, scope="main-nn", **kwargs)
+
+        self.concat_layer = ConcatLayer()
+        sub_components = ([self.image_stack] if self.image_stack is not None else []) + \
+                         [self.concat_layer, self.main_nn]
+        super(QFunction, self).__init__(*sub_components, num_inputs=2, scope=kwargs.pop("scope", "q-function"), **kwargs)
 
     @rlgraph_api
-    def state_action_value(self, states, actions, internal_states=None):
+    def call(self, states, actions):
         """
         Computes Q(s,a) by passing states and actions through one or multiple processing stacks..
         """
+        # Send states through separate stack, only then concat.
         if self.image_stack is not None:
             image_processing_output = self.image_stack.call(states)
-            state_actions = self.concat_layer.call(image_processing_output, actions)
-            dense_output = self.dense_stack.call(state_actions)
+            concat_input = (image_processing_output, actions)
+        # Concat states and actions, then pass through.
         else:
-            # Concat states and actions, then pass through.
-            state_actions = self.concat_layer.call(states, actions)
-            dense_output = self.dense_stack.call(state_actions)
+            concat_input = (states, actions)
 
-        return dense_output
+        states_and_actions = self.concat_layer.call(concat_input)
+        return self.main_nn.call(states_and_actions)
